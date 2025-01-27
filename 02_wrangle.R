fdb2fa15c7b90ecb059998c554597371fc1c711a
njlyon0 <nicholasjlyon@gmail.com>
2025-01-27 15:34:31 -0800

    feat (wrangle): treatment data parsing

--- 02_wrangle.R
+++ 02_wrangle.R
@@ -23,8 +23,44 @@ tidy_v1 <- read.csv(file.path("data", "caged_harmonized.csv"))
 dplyr::glimpse(tidy_v1)
 
 ## ------------------------------------------- ##
+# Treatments ----
+## ------------------------------------------- ##
+
+# Combine/streamline treatment information
+tidy_v2 <- tidy_v1 %>% 
+  # Combine into a single treatment column
+  dplyr::mutate(
+    original.treatment = dplyr::case_when(
+      ## Use central treatment (if exists)
+      nchar(orig.treat) != 0 ~ orig.treat,
+      ## Combine fire/fence/gap for relevant study
+      source == "royo_westvirginia_fernow_2000-2013_deer_plants.csv" ~ paste(orig.treat_fire, orig.treat_fence, orig.treat_gap, sep = "; "),
+      ## Otherwise, put in warning text
+      T ~ "NO TREATMENT IDENTIFIED"),
+    .before = orig.treat) %>% 
+  # Drop now superseded precursor columns
+  dplyr::select(-dplyr::contains("orig.treat"))
+  
+# Check structure
+dplyr::glimpse(tidy_v2)
+
+
 
 ## ------------------------------------------- ##
+# Export ----
+## ------------------------------------------- ##
+
+# Final pre-export tweaks
+tidy_v3 <- tidy_v2 %>% 
+  # Drop duplicate rows
+  dplyr::distinct()
+
+# Check structure
+dplyr::glimpse(tidy_v3)
+
+# Export locally
+write.csv(x = tidy_v3, row.names = F, na = '',
+          file = file.path("data", "caged_tidied.csv"))
 
 
 # End ----
