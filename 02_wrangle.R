3e9d1bc2bef85d72cb77f3223e5e03f25f746aaf
njlyon0 <nicholasjlyon@gmail.com>
2025-01-27 17:05:08 -0800

    feat (wrangle): moved treatment consolidation to harmonization script

--- 02_wrangle.R
+++ 02_wrangle.R
@@ -41,28 +41,6 @@ tidy_v2 <- tidy_v1 %>%
 dplyr::glimpse(tidy_v2)
 
 ## ------------------------------------------- ##
-# Treatments ----
-## ------------------------------------------- ##
-
-# Combine/streamline treatment information
-tidy_v3 <- tidy_v2 %>% 
-  # Combine into a single treatment column
-  dplyr::mutate(
-    original.treatment = dplyr::case_when(
-      ## Use central treatment (if exists)
-      nchar(orig.treat) != 0 ~ orig.treat,
-      ## Combine fire/fence/gap for relevant study
-      source == "royo_westvirginia_fernow_2000-2013_deer_plants.csv" ~ paste(orig.treat_fire, orig.treat_fence, orig.treat_gap, sep = "; "),
-      ## Otherwise, put in warning text
-      T ~ "NO TREATMENT IDENTIFIED"),
-    .before = orig.treat) %>% 
-  # Drop now superseded precursor columns
-  dplyr::select(-dplyr::contains("orig.treat"))
-  
-# Check structure
-dplyr::glimpse(tidy_v3)
-
-## ------------------------------------------- ##
 # Export ----
 ## ------------------------------------------- ##
 
