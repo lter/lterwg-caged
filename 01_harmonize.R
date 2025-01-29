6379a37b53a8a44c2514ac5a730b89f36b9ca583
njlyon0 <nicholasjlyon@gmail.com>
2025-01-29 10:11:04 -0800

    feat (harmonize): uploading harmonized data to Drive

--- 01_harmonize.R
+++ 01_harmonize.R
@@ -319,6 +319,8 @@ dplyr::glimpse(combo_v6)
 write.csv(x = combo_v6, row.names = F, na = '',
           file = file.path("data", "caged_harmonized.csv"))
 
-
+# Upload to Drive
+googledrive::drive_upload(media = file.path("data", "caged_harmonized.csv"), overwrite = T,
+                          path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Acv2ybcpOd_8jEohzgVWcm5qRmgDb4Od"))
 
 # End ----
