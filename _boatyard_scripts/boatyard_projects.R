5bb3e7141915864bcd39035b0a8281cb6f98670b
njlyon0 <nicholasjlyon@gmail.com>
2025-01-30 09:52:11 -0800

    fix: repaired housekeeping section post-conflict in boatyard

--- _boatyard_scripts/boatyard_projects.R
+++ _boatyard_scripts/boatyard_projects.R
@@ -1,22 +1,28 @@
 ## --------------------------------------------------------------- ##
 # CAGED Harmonization Workflow
 ## --------------------------------------------------------------- ##
-
 # Written by: Kelly Speare, Nick J Lyon, ...
 
-## ------------------------------------------- ##
+# Purpose
+## this script downloads data from purgatory folder in google drive for all data files that require rangling
+## then does necessary wrangling to get the data in the needed format
+## then uploads back to google drive
 
+## ------------------------------------------- ##
 # Housekeeping ----
-
 ## ------------------------------------------- ##
-
-library(tidyverse)
 
 # Load libraries
-
 librarian::shelf(tidyverse, googledrive)
 
-# Purpose
+# Create needed folder(s)
+dir.create(path = file.path("data"), showWarnings = F)
+dir.create(path = file.path("data", "purgatory"), showWarnings = F)
+dir.create(path = file.path("data", "drydock"), showWarnings = F)
+
+# Clear environment + collect garbage
+rm(list = ls()); gc()
+
 ## ------------------------------------------- ##
 # Download Data ----
 ## ------------------------------------------- ##
