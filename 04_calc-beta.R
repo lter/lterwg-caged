bb3d97afa033b9c8a5efbe08147bec92ac9d2383
njlyon0 <nicholasjlyon@gmail.com>
2025-02-04 12:13:40 -0500

    dev (beta): partially complete beta dispersion calculation workflow

--- 04_calc-beta.R
+++ 04_calc-beta.R
@@ -26,6 +26,72 @@ dplyr::glimpse(beta_v1)
 # Calculate Beta Dispersion ----
 ## ------------------------------------------- ##
 
+# Create a list for storing outputs
+beta_list <- list()
+
+# Loop across original data source
+for(focal_src in unique(beta_v1$source)){
+  
+  # Progress message
+  message("Processing source '", focal_src, "'")
+  
+  # Subset data
+  src_sub <- beta_v1 %>% 
+    dplyr::filter(source == focal_src)
+  
+  # Loop across treatments
+  for(focal_trt in unique(src_sub$original.treatment)){
+    
+    # Subset again
+    trt_sub <- src_sub %>% 
+      dplyr::filter(original.treatment == focal_trt)
+    
+    # Loop across most granular level of experimental design
+    for(focal_des1 in unique(trt_sub$exp.design.1)){
+      
+      # Subset yet again
+      des1_sub <- trt_sub %>% 
+        dplyr::filter(exp.design.1 == focal_des1)
+      
+      # Pivot to wide format & drop all empty columns
+      des1_sub_wide <- des1_sub %>% 
+        tidyr::pivot_wider(names_from = original.taxa,
+                           values_from = abundance) %>% 
+        dplyr::select(-source:-distance.from.source)
+      
+      # Get distance/dissimilarity matrix
+      des1_sub_dist <- vegan::vegdist(x = des1_sub_wide, method = "bray")
+      
+      # Calculate beta dispersion
+      des1_sub_beta <- vegan::betadisper(d = des1_sub_dist,
+                                         group = as.factor(rep(x = "x", 
+                                                               times = nrow(des1_sub_wide))),
+                                         type = "centroid", bias.adjust = F, 
+                                         sqrt.dist = F, add = F)
+      # Prepare output(s)
+      des1_sub_out <- des1_sub %>% 
+        dplyr::select(source:distance.from.source) %>% 
+        dplyr::distinct() %>% 
+        dplyr::mutate(exp.design.1.n = nrow(des1_sub_wide),
+                      exp.design.1.betadisp = des1_sub_beta$distances)
+      
+      # Add to list
+      beta_list[[paste0(focal_src, focal_trt, focal_des1)]] <- des1_sub_out
+      
+    } # Close "exp.design.1" loop
+  } # Close treatment loop
+} # Close source loop
+
+# Unlist the list
+beta_v2 <- purrr::list_rbind(x = beta_list)
+
+# Check structure
+dplyr::glimpse(beta_v2)
+
+
+str(trt_sub)
+
+
 # The group suggests the following function:
 ?vegan::betadisper
 
