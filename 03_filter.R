97d9deb5397ea30dd53b934364cc9a8c6820be7a
njlyon0 <nicholasjlyon@gmail.com>
2025-02-20 09:45:41 -0500

    feat (filter): removing columns standardized in prior script and updating downstream object numbering

--- 03_filter.R
+++ 03_filter.R
@@ -23,18 +23,33 @@ sub_v1 <- read.csv(file.path("data", "02_caged_tidied.csv"))
 dplyr::glimpse(sub_v1)
 
 ## ------------------------------------------- ##
-# Drop Zero-Abundance Samples ----
+# Drop Unwanted Columns ----
 ## ------------------------------------------- ##
 
 # Check structure
 dplyr::glimpse(sub_v1)
 
-# Remove 'exp.design.1' levels without any abundance
+# Drop any columns we know we don't want at the outset
 sub_v2 <- sub_v1 %>% 
+  # Supersesed "original" columns (standardized in QC script)
+  dplyr::select(-original.treatment, -original.taxa)
+
+# Re-check structure
+dplyr::glimpse(sub_v2)
+
+## ------------------------------------------- ##
+# Drop Zero-Abundance Samples ----
+## ------------------------------------------- ##
+
+# Check structure
+dplyr::glimpse(sub_v2)
+
+# Remove 'exp.design.1' levels without any abundance
+sub_v3 <- sub_v2 %>% 
   # Average abundance withing experimental design level 1
   dplyr::group_by(
     dplyr::across(dplyr::all_of(setdiff(x = names(.),
-                                        y = c("original.taxa", "abundance"))))
+                                        y = c("taxa", "abundance"))))
   ) %>% 
   dplyr::mutate(avg.abun = mean(abundance, na.rm = T)) %>% 
   dplyr::ungroup() %>% 
@@ -44,28 +59,28 @@ sub_v2 <- sub_v1 %>%
   dplyr::select(-avg.abun)
 
 # Check number of lost rows
-message(nrow(sub_v1) - nrow(sub_v2), " rows lost")
+message(nrow(sub_v2) - nrow(sub_v3), " rows lost")
 
 # Identify any datasets dropped entirely (shouldn't be any)
-setdiff(x = unique(sub_v1$source), y = unique(sub_v2$source))
+setdiff(x = unique(sub_v2$source), y = unique(sub_v3$source))
 
 # Re-check structure
-dplyr::glimpse(sub_v2)
+dplyr::glimpse(sub_v3)
 
 ## ------------------------------------------- ##
 # Handle Sub-Annual Sampling ----
 ## ------------------------------------------- ##
 
 # Check structure
-dplyr::glimpse(sub_v2)
+dplyr::glimpse(sub_v3)
 
 # Do needed processing
-sub_v3 <- sub_v2 %>% 
+sub_v4 <- sub_v3 %>% 
   # Identify cases with more than one sampling point within dataset/year
   dplyr::group_by(source, year) %>% 
   dplyr::mutate(time.ct = length(unique(sampling.point))) %>% 
   dplyr::ungroup() %>% 
-  # Filter to only either the _last_ sampling point or any dataset without that info
+  # Filter to only either the _last_ sampling point or any dataset without sub-annual sampling
   dplyr::filter(
     time.ct == 1 |
       (source == "gilson_southafrica_intertidalexclusion_2021_grazers_algae.csv" & 
@@ -81,20 +96,20 @@ sub_v3 <- sub_v2 %>%
   dplyr::select(-sampling.point, -time.ct)
 
 # Check number of lost rows
-message(nrow(sub_v2) - nrow(sub_v3), " rows lost")
+message(nrow(sub_v3) - nrow(sub_v4), " rows lost")
 
 # Identify any datasets dropped entirely (shouldn't be any)
-setdiff(x = unique(sub_v2$source), y = unique(sub_v3$source))
+setdiff(x = unique(sub_v3$source), y = unique(sub_v4$source))
 
 # Re-check structure
-dplyr::glimpse(sub_v3)
+dplyr::glimpse(sub_v4)
 
 ## ------------------------------------------- ##
 # Export ----
 ## ------------------------------------------- ##
 
 # Create final object name
-sub_v99 <- sub_v3
+sub_v99 <- sub_v4
 
 # Identify tidy file name / path
 filter_name <- "03_caged_filtered.csv"
