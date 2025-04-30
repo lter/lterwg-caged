4db9617019c5429d19be191d7d97c41296ea417b
njlyon0 <nicholasjlyon@gmail.com>
2025-04-30 15:57:39 -0400

    fix (harmonize): repairing hasty prior fix (for removal of "." as a custom NA value used in some datasets)

--- 01_harmonize.R
+++ 01_harmonize.R
@@ -281,7 +281,7 @@ combo_v5 <- combo_v4 %>%
   # Remove any rows where no taxon information is included
   dplyr::filter(is.na(original.taxa) != T) %>% 
   # Remove non-numbers
-  dplyr::mutate(abundance = gsub(pattern = "\\.", replacement = "", x = abundance)) %>% 
+  dplyr::mutate(abundance = gsub(pattern = "^\\.$", replacement = "", x = abundance)) %>% 
   dplyr::mutate(abundance = ifelse(test = abundance %in% c("n/a", "—", "na"),
                                    yes = "", no = abundance)) %>% 
   # Remove any rows where no metric of abundance is included
