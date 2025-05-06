fe5f59979ccf937b1c6292a305f5ad7cec6d1739
njlyon0 <nicholasjlyon@gmail.com>
2025-05-06 07:11:42 -0700

    refactor (attach meta): standardizing lat/long format in metadata before joining (rather than after joining)

--- 06_attach-metadata.R
+++ 06_attach-metadata.R
@@ -51,65 +51,14 @@ meta_v1 <- read.csv(file = file.path("data", "sitelevel-metadata.csv"))
 dplyr::glimpse(meta_v1)
 
 ## ------------------------------------------- ##
-# Check Join Keys for Mismatches ----
-## ------------------------------------------- ##
-
-# Check for mismatches in which datasets are in the data but not metadata (or vice versa)
-supportR::diff_check(old = unique(w.meta_v1$source), new = unique(meta_v1$source))
-## If any are in data but not *metadata*:
-### Run "_boatyard_scripts/expand_metadata.R" and follow instructions at end of script
-
-## If any are in metadata but not *data*:
-### For some reason no beta dispersion was calculated for any spatial level
-### (likely lack of "exp.design" columns in original dataset)
-### Check data key to confirm
-
-# Remove any files not found in the data from the metadata
-meta_v2 <- dplyr::filter(.data = meta_v1, source %in% w.meta_v1$source)
-
-# Now check for mismatches in "exp.name" column
-## This is why this metadata is "site level"
-supportR::diff_check(old = unique(w.meta_v1$exp.name), new = unique(meta_v2$exp.name))
-## If any are in data but not *metadata*:
-### The metadata had this info entered incorrectly
-### Open the GoogleSheet and edit the "exp.name" column as needed
-### Once done, start running this script again from the top to re-download the fixed version
-
-## If any are in metadata but not *data*:
-### Again, for some reason, no beta dispersion was calculated
-### Check original data and beta dispersion calculation script to debug
-
-# Remove any experiment names not found in data
-meta_v3 <- dplyr::filter(.data = meta_v2, exp.name %in% w.meta_v1$exp.name)
-
-# Re-check that there are no mismatches
-supportR::diff_check(old = unique(w.meta_v1$source), new = unique(meta_v3$source))
-supportR::diff_check(old = unique(w.meta_v1$exp.name), new = unique(meta_v3$exp.name))
-
-## ------------------------------------------- ##
-# Join Metadata ----
-## ------------------------------------------- ##
-
-# Actually join the metadata with the 'actual' data
-w.meta_v2 <- w.meta_v1 %>% 
-  dplyr::left_join(y = meta_v3, by = c("source", "exp.name")) %>% 
-  # Relocate all of these columns more intuitively
-  dplyr::relocate(assigned.to:notes, .after = exp.name) %>% 
-  # Drop likely unwanted columns
-  dplyr::select(-assigned.to, -notes)
-
-# Check structure
-dplyr::glimpse(w.meta_v2)
-
-## ------------------------------------------- ##
 # Standardize Lat/Long Format ----
 ## ------------------------------------------- ##
 
 # Check current lat/long formats
-sort(unique(w.meta_v2$lat))
+sort(unique(meta_v1$lat))
 
 # Do needed repairs
-w.meta_v3 <- w.meta_v2 %>% 
+meta_v2 <- meta_v1 %>% 
   # Rename & duplicate original lat/long cols
   dplyr::mutate(lat.orig = lat,
                 long.orig = long) %>% 
@@ -153,31 +102,82 @@ w.meta_v3 <- w.meta_v2 %>%
   dplyr::relocate(lat.orig:long, .after = exp.name)
 
 # Re-check formats
-sort(unique(w.meta_v3$lat))
+sort(unique(meta_v2$lat))
 
 # Check structure more generally
-dplyr::glimpse(w.meta_v3)
+dplyr::glimpse(meta_v2)
+
+## ------------------------------------------- ##
+# Check Join Keys for Mismatches ----
+## ------------------------------------------- ##
+
+# Check for mismatches in which datasets are in the data but not metadata (or vice versa)
+supportR::diff_check(old = unique(w.meta_v1$source), new = unique(meta_v2$source))
+## If any are in data but not *metadata*:
+### Run "_boatyard_scripts/expand_metadata.R" and follow instructions at end of script
+
+## If any are in metadata but not *data*:
+### For some reason no beta dispersion was calculated for any spatial level
+### (likely lack of "exp.design" columns in original dataset)
+### Check data key to confirm
+
+# Remove any files not found in the data from the metadata
+meta_v3 <- dplyr::filter(.data = meta_v2, source %in% w.meta_v1$source)
+
+# Now check for mismatches in "exp.name" column
+## This is why this metadata is "site level"
+supportR::diff_check(old = unique(w.meta_v1$exp.name), new = unique(meta_v3$exp.name))
+## If any are in data but not *metadata*:
+### The metadata had this info entered incorrectly
+### Open the GoogleSheet and edit the "exp.name" column as needed
+### Once done, start running this script again from the top to re-download the fixed version
+
+## If any are in metadata but not *data*:
+### Again, for some reason, no beta dispersion was calculated
+### Check original data and beta dispersion calculation script to debug
+
+# Remove any experiment names not found in data
+meta_v4 <- dplyr::filter(.data = meta_v3, exp.name %in% w.meta_v1$exp.name)
+
+# Re-check that there are no mismatches
+supportR::diff_check(old = unique(w.meta_v1$source), new = unique(meta_v4$source))
+supportR::diff_check(old = unique(w.meta_v1$exp.name), new = unique(meta_v4$exp.name))
+
+## ------------------------------------------- ##
+# Join Metadata ----
+## ------------------------------------------- ##
+
+# Actually join the metadata with the 'actual' data
+w.meta_v2 <- w.meta_v1 %>% 
+  dplyr::left_join(y = meta_v4, by = c("source", "exp.name")) %>% 
+  # Relocate all of these columns more intuitively
+  dplyr::relocate(assigned.to:notes, .after = exp.name) %>% 
+  # Drop likely unwanted columns
+  dplyr::select(-assigned.to, -notes)
+
+# Check structure
+dplyr::glimpse(w.meta_v2)
 
 ## ------------------------------------------- ##
 # Standardize Free Text Columns ----
 ## ------------------------------------------- ##
 
 # This is harder to do extensively but some coarse stuff makes sense
-w.meta_v4 <- w.meta_v3 %>% 
+w.meta_v3 <- w.meta_v2 %>% 
   # Make some columns lowercase
   dplyr::mutate(dplyr::across(.cols = c(ecotype2, target.consumer, nontarget.consumer,
                                         resource.type, consumer.trophic.level),
                               .fns = ~ tolower(x = .)))
 
 # Re-check structure
-dplyr::glimpse(w.meta_v4)
+dplyr::glimpse(w.meta_v3)
 
 ## ------------------------------------------- ##
 # Export ----
 ## ------------------------------------------- ##
 
 # Create final object name
-w.meta_v99 <- w.meta_v4
+w.meta_v99 <- w.meta_v3
 
 # Identify tidy file name / path
 w.meta_name <- "06_caged_with-metadata.csv"
