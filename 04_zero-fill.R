ec80f3012e70831b462bccdfee64b80e44d43f28
njlyon0 <nicholasjlyon@gmail.com>
2025-05-07 10:32:55 -0700

    feat (zero fill): adding diagnostic check for rows we summarize across pre-zero filling

--- 04_zero-fill.R
+++ 04_zero-fill.R
@@ -37,7 +37,6 @@ dplyr::glimpse(fill_v1)
 ## we'd just be creating a bunch of useless rows and blowing up the size of file coming out of this script
 ### (beyond how huge it will be if it works as designed!)
 
-
 # Summarize to only one replicate within the finest design scale
 ## Standardization of treatments alone will result in "duplicates" across which we'd want to average
 fill_v2 <- fill_v1 %>% 
@@ -45,31 +44,13 @@ fill_v2 <- fill_v1 %>%
     dplyr::across(
       dplyr::all_of(setdiff(x = names(fill_v1), y = "abundance")))) %>% 
   dplyr::summarize(abundance = mean(abundance, na.rm = T),
-                   # count the number of rows (we want this to be 1)
-                   count=dplyr::n(),
-                   # this lets you know what the multiple values are 
-                   abunda.vals=paste(abundance, collapse="; "),
-                   sd.abund =sd(abundance, na.rm=T),
                    .groups = "keep") %>% 
-  dplyr::ungroup() %>%
-  # filter out the problem datasets that have more than one value 
-  filter(count >1)
-
-view(fill_v2)
-
-# Which datasets are the problem?
-fill_v2 %>%
-  pull(source) %>%
-  unique()
-
-
+  dplyr::ungroup()
 
 # Why are there duplicate values?
-# sometimes datasets have a blank value and an abundance value, so then it will have two rows, and if we average
-# across them, it will fix the issue (blank is not zero!)
-
-
-
+## Sometimes datasets have a blank value and an abundance value
+## So then it will have two rows, and if we average across them, it will fix the issue
+### (blank is not zero!!!)
 
 # How many rows were summarized across?
 message(nrow(fill_v1) - nrow(fill_v2), " rows lost by summarizing within 'exp.design.1'")
@@ -80,7 +61,29 @@ message(nrow(fill_v1) - nrow(fill_v2), " rows lost by summarizing within 'exp.de
 # Identify any datasets dropped entirely (shouldn't be any)
 supportR::diff_check(old = unique(fill_v1$source), new = unique(fill_v2$source))
 
-# Re-check structure
+# Where are we losing rows (i.e., summarizing across abundance values)?
+lost_rows <- fill_v1 %>% 
+  # Group in the same way as above
+  dplyr::group_by(
+    dplyr::across(
+      dplyr::all_of(setdiff(x = names(fill_v1), y = "abundance")))) %>%
+  # Get some nice diagnostic columns
+  dplyr::summarize(mean.abun = mean(abundance, na.rm = T),
+                   sd.abun = sd(abundance, na.rm = T),
+                   # Check what the values are that we average across
+                   abun.vals = paste(abundance, collapse = "; "),
+                   # Count the number of rows (we want this to be 1)
+                   row.count = dplyr::n(),
+                   .groups = "keep") %>% 
+  dplyr::ungroup() %>%
+  # Filter to only the datasets where we are losing rows
+  dplyr::filter(row.count > 1)
+  
+# Check that out
+dplyr::glimpse(lost_rows)
+## View(lost_rows)
+
+# Re-check structure of filled object
 dplyr::glimpse(fill_v2)
 
 ## ------------------------------------------- ##
