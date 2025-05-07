f2d4508ca5c5c210306fc0858c8ea2ccaea0705e
njlyon0 <nicholasjlyon@gmail.com>
2025-05-07 14:50:16 -0700

    feat: updating raw data folder Drive link

--- 01_harmonize.R
+++ 01_harmonize.R
@@ -27,7 +27,7 @@ rm(list = ls()); gc()
 ### https://lter.github.io/scicomp/tutorial_googledrive-pkg.html
 
 # Identify wanted files
-files_drive <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1EOSlNF3zz-ktBQwoIt1a30dv0azJ1g5M")) %>% 
+files_drive <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/u/0/folders/1E11bCAJQ8UzV80s1tf4KC4kiTa5fRwCX")) %>% 
   dplyr::filter(stringr::str_detect(string = .$name, pattern = "\\.csv"))
 
 # Did that work?
