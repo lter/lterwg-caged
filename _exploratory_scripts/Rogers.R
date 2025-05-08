875eae2bf4decc61281be8de3b63ed1a64c8c7f1
ryancr29 <ryancr@stanford.edu>
2025-05-08 11:41:32 -0700

    Graph code, mean beta diffs x [INSERT Fixed effect]

--- _exploratory_scripts/Rogers.R
+++ _exploratory_scripts/Rogers.R
@@ -2,11 +2,12 @@
 ## --------------------------------------------------------------- ##
 # CAGED Beta disp.diff figs
 ## --------------------------------------------------------------- ##
-# Written by: Ryan Rogers
+#Cannibalized from Nick, Jamie, Luisa, Marc
+#Written by: Ryan Rogers
 
-#Figure(s) of ecosystem types x difference (bets diversity, dispersion)
+#Figure(s) beta diversity mean difference
 
-#Aquatic vs terrestiral comaprisons, 
+#Aquatic vs terrestrial comparisons, 
 #Caged treatment standardization, consumer richness.category, lat
 
 #################################################
@@ -35,7 +36,9 @@ caged_v2 <- caged_v1 %>%
   dplyr::filter(cage.treatment_std %in% c("caged", "uncaged")) %>% 
   # Summarize
   # Summarize within treatments
-  dplyr::group_by(source, betadisp.design.level, cage.treatment_std) %>% 
+  dplyr::group_by(source, exp.name, cage.treatment_std,
+                  lat, ecotype1, aq.or.terr, exclusion.duration,
+                  climate.zone, consumer.richness.category, natural.vs.artificial.substrate) %>% 
   dplyr::summarize(betadisp.mean = mean(betadisp.comm.dist, na.rm = T),
                    .groups = "keep") %>% 
   dplyr::ungroup() %>% 
@@ -45,22 +48,36 @@ caged_v2 <- caged_v1 %>%
   # Calculate difference
   dplyr::mutate(diff = uncaged - caged)
 
+
+#standardize exclusion.duration column
+caged_v2$exclusion.duration #many different formats, needs to be cleaned - I'm not sure if thats the best way to go
+
+caged_v3 <- caged_v2 %>%
+  mutate( 
+    exclusion.digits = as.numeric(str_extract(exclusion.duration, "\\d+")),
+    exclusion.duration.clean = case_when(
+      # Convert years to months
+      str_detect(exclusion.duration, "year") ~ exclusion.digits * 12,
+      
+      # Convert weeks to months (approximate)
+      str_detect(exclusion.duration, "week") ~ exclusion.digits / 4.345,
+      
+      # Keep months as-is
+      str_detect(exclusion.duration, "month") ~ exclusion.digits,
+      
+      # Plain numbers assumed to be months
+      # str_detect(exclusion.duration, "^\\d+$") ~ as.numeric(exclusion.duration), ####risky
+      
+      # Everything else becomes NA
+      TRUE ~ NA_real_
+    )
+  ) %>% select(-exclusion.digits)
+
+#checking
+sort(unique(caged_v3$exclusion.duration.clean))
+
 # Re-check structure
-dplyr::glimpse(caged_v2)
-
-## ------------------------------------------- ##
-# Nick's Create Graph (Across Design Levels) ----
-## ------------------------------------------- ##
-
-# Create desired graph
-ggplot(caged_v2, aes(x = diff, y = reorder(source, dplyr::desc(-diff)), 
-                     color = betadisp.design.level)) +
-  geom_point() +
-  geom_vline(xintercept = 0, linetype = 3) +
-  labs(x = "Uncaged - Caged Beta Dispersion",
-       y = "Dataset Source") +
-  supportR::theme_lyon() +
-  theme(axis.text.y = element_blank())
+dplyr::glimpse(caged_v3)
 
 #### re-attach metadata ####
 
@@ -70,15 +87,18 @@ caged_v3$natural.vs.artificial.substrate <- as.factor(caged_v3$natural.vs.artifi
 
 dplyr::glimpse(caged_v3)
 
+###########
+#Graphs/Plots Below
+
 #### Exploratory plots, diff x gamma rich (?), ecosystem type, cage size, herbivore rich, latitude
 
 ### Figure 1 Successional stage ####
 
-ggplot(caged_v3, aes(x = natural.vs.artificial.substrate, y = diff)) +
+succdiffplot <- ggplot(caged_v3, aes(x = natural.vs.artificial.substrate, y = diff)) +
   geom_boxplot(aes(fill = natural.vs.artificial.substrate), alpha = 0.4) +
   #geom_jitter(aes(fill = natural.vs.artificial.substrate), width = 0.15,
-              #size = 2.5, pch = 21) +
-  labs(x = "Natural vs. artificial substrate (Succession)", y = "diff",
+             # size = 2.5, pch = 21) +
+  labs(x = "Natural vs. artificial substrate (Succession)", y = "Beta diff (Uncaged-Caged)",
        title = paste0("Graph created on ", Sys.Date())) +
   #scale_fill_manual(values = c("caged" = "red", "uncaged" = "blue", "partial" = "purple", 
   #                            "unknown" = "gray", "uncertain" = "gray20")) +
@@ -88,12 +108,43 @@ ggplot(caged_v3, aes(x = natural.vs.artificial.substrate, y = diff)) +
         axis.text.x = element_text(angle = 35, hjust = 1)) +
   supportR::theme_lyon()
 
+succdiffplot
+
 ### Figure 2 Ecosystem type (coarse) - Terrestrial/Aquatic ####
 
-ggplot(caged_v3)
+coarecodifplot <- ggplot(caged_v3, aes(x = aq.or.terr, y = diff)) +
+  geom_boxplot(aes(fill = aq.or.terr), alpha = 0.4) +
+  #geom_jitter(aes(fill = natural.vs.artificial.substrate), width = 0.15,
+  # size = 2.5, pch = 21) +
+  labs(x = "Ecosystem type (coarse)", y = "Beta dis. diff (Uncaged - Caged)",
+       title = paste0("Graph created on ", Sys.Date())) +
+  #scale_fill_manual(values = c("caged" = "red", "uncaged" = "blue", "partial" = "purple", 
+  #                            "unknown" = "gray", "uncertain" = "gray20")) +
+  theme(legend.position = "none",
+        legend.title = element_blank(),
+        strip.text = element_text(size = 8),
+        axis.text.x = element_text(angle = 35, hjust = 1)) +
+  supportR::theme_lyon()
+
+coarecodifplot
 
 
 #### Figure 3 Ecosystem type (fine) - Ecosystem types ####
+
+#remove NAs for now
+
+ecotypediff.boxplot <-
+  caged_v3 %>% 
+  ggplot(data = ., aes(x = ecotype1, y = diff)) + 
+  geom_boxplot() +
+  # geom_jitter(height = 0, width = 0.1, size = .5, alpha = 0.4) +
+  labs(y = expression("Beta diff (Uncaged - Caged)"), x = "") +
+  theme_bw(base_size=12)  +
+  theme(plot.margin = unit(c(1,1,1,1), "cm"), 
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "", legend.title = element_blank())
+  #facet_wrap(~ecotype1, scale = "free_y")
+
+ecotypediff.boxplot
 
 ### Figure 4 Gamma richness ####
 
@@ -102,9 +153,6 @@ ggplot(caged_v3)
 #### Figure 6 Exclusion time #####
 
 #### Figure 7 Size of cage ####
-
-
-
 
 
 
