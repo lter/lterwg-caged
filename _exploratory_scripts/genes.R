47520560c286dcdac22d14424ec5e66f7d6a367d
luisagenes <lgenes@stanford.edu>
2025-05-08 14:01:32 -0700

    gamma div general

--- _exploratory_scripts/genes.R
+++ _exploratory_scripts/genes.R
@@ -325,6 +325,26 @@ ggplot(alldata_v2, aes(x = gamma.richness, y =betadisp.comm.dist, color = aq.or.
   theme_classic() + 
   theme(panel.border = element_rect(color = "black", fill = NA, linewidth = 1))
 
+#general
+ggplot(alldata_v2, aes(x = gamma.richness, y =betadisp.comm.dist)) +
+  geom_point(alpha = 0.7) +
+  geom_smooth(method = "lm", se = T) +
+  labs(x = "Gamma diversity", y = "beta.disp.comm.dist") +
+ # scale_color_manual(values = c("aquatic" = "darkblue", "terrestrial" = "green4")) + 
+  facet_wrap(~ cage.treatment_std)+
+  theme_classic() + 
+  theme(panel.border = element_rect(color = "black", fill = NA, linewidth = 1))
+
+ggplot(alldata_v2, aes(x = gamma.richness, y =betadisp.comm.dist, fill=cage.treatment_std, color=cage.treatment_std)) +
+  geom_point(alpha = 0.7) +
+  geom_smooth(method = "lm", se = T) +
+  labs(x = "Gamma diversity", y = "beta.disp.comm.dist") +
+   scale_fill_manual(values = c("uncaged" = "purple1", "caged" = "grey39")) +
+  scale_color_manual(values = c("uncaged" = "purple1", "caged" = "grey39"))+
+ # facet_wrap(~ cage.treatment_std)+
+  theme_classic() + 
+  theme(panel.border = element_rect(color = "black", fill = NA, linewidth = 1))
+
 
 
 
