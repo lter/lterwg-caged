a9bc4151b49ce866ea90ee2912d1e2fb4165a243
njlyon0 <nicholasjlyon@gmail.com>
2025-05-08 12:10:17 -0700

    feat (attach meta): first complete pass at integrating various ancillary data files and exporting outputs with new numbering

--- 07_attach-metadata.R
+++ 07_attach-metadata.R
@@ -25,12 +25,12 @@ rm(list = ls()); gc()
 
 # Identify data files we want to add stuff to
 (w.meta_outs <- dir(path = file.path("data"), pattern = "05-A_caged_beta-disp_"))
-w.meta_list <- purrr::map(.x = w.meta_outs,
+w.meta_in_list <- purrr::map(.x = w.meta_outs,
                           .f = ~ read.csv(file = file.path("data", .x)))
-names(w.meta_list) <- w.meta_outs
+names(w.meta_in_list) <- w.meta_outs
 
 # Check structure of one
-dplyr::glimpse(w.meta_list[[1]])
+dplyr::glimpse(w.meta_in_list[[1]])
 
 ## ------------------------------------------- ##
 # Download Metadata ----
@@ -143,8 +143,8 @@ dplyr::glimpse(meta_v4)
 ## ------------------------------------------- ##
 
 # Check for mismatches in which datasets are in the data but not metadata (or vice versa)
-supportR::diff_check(old = unique(c(w.meta_list[[1]]$source,
-                                    w.meta_list[[2]]$source)), 
+supportR::diff_check(old = unique(c(w.meta_in_list[[1]]$source,
+                                    w.meta_in_list[[2]]$source)), 
                      new = unique(meta_v3$source))
 ## If any are in data but not *metadata*:
 ### Run "_boatyard_scripts/expand_metadata.R" and follow instructions at end of script
@@ -156,13 +156,13 @@ supportR::diff_check(old = unique(c(w.meta_list[[1]]$source,
 ### (or potentially removed due to confounding treatments)
 
 # Remove any files not found in the data from the metadata
-meta_v5 <- dplyr::filter(.data = meta_v4, source %in% unique(c(w.meta_list[[1]]$source,
-                                                               w.meta_list[[2]]$source)))
+meta_v5 <- dplyr::filter(.data = meta_v4, source %in% unique(c(w.meta_in_list[[1]]$source,
+                                                               w.meta_in_list[[2]]$source)))
 
 # Now check for mismatches in "exp.name" column
 ## This is why this metadata is "site level"
-supportR::diff_check(old = unique(c(w.meta_list[[1]]$exp.name,
-                                    w.meta_list[[2]]$exp.name)),
+supportR::diff_check(old = unique(c(w.meta_in_list[[1]]$exp.name,
+                                    w.meta_in_list[[2]]$exp.name)),
                      new = unique(meta_v5$exp.name))
 ## If any are in data but not *metadata*:
 ### The metadata had this info entered incorrectly
@@ -174,15 +174,15 @@ supportR::diff_check(old = unique(c(w.meta_list[[1]]$exp.name,
 ### Check original data and beta dispersion calculation script to debug
 
 # Remove any experiment names not found in data
-meta_v6 <- dplyr::filter(.data = meta_v5, exp.name %in% unique(c(w.meta_list[[1]]$exp.name,
-                                                                 w.meta_list[[2]]$exp.name)))
+meta_v6 <- dplyr::filter(.data = meta_v5, exp.name %in% unique(c(w.meta_in_list[[1]]$exp.name,
+                                                                 w.meta_in_list[[2]]$exp.name)))
 
 # Re-check that there are no mismatches
-supportR::diff_check(old = unique(c(w.meta_list[[1]]$source,
-                                    w.meta_list[[2]]$source)),
+supportR::diff_check(old = unique(c(w.meta_in_list[[1]]$source,
+                                    w.meta_in_list[[2]]$source)),
                      new = unique(meta_v6$source))
-supportR::diff_check(old = unique(c(w.meta_list[[1]]$exp.name,
-                                    w.meta_list[[2]]$exp.name)),
+supportR::diff_check(old = unique(c(w.meta_in_list[[1]]$exp.name,
+                                    w.meta_in_list[[2]]$exp.name)),
                                   new = unique(meta_v6$exp.name))
 
 ## ------------------------------------------- ##
@@ -223,6 +223,9 @@ dplyr::glimpse(diff_v1)
 ## ------------------------------------------- ##
 ## https://tenor.com/view/everyone-the-professional-shout-gif-12696023
 
+# Make a list for storing outputs
+w.meta_out_list <- list()
+
 # Loop across files for which we want 'metadata' attached
 for(focal_w.meta in w.meta_outs){
   
@@ -230,11 +233,11 @@ for(focal_w.meta in w.meta_outs){
   message("Attaching ancillary data to ", focal_w.meta)
   
   # Grab just that file out of the list of inputs
-  w.meta_v1 <- w.meta_list[[focal_w.meta]]
+  w.meta_v1 <- w.meta_in_list[[focal_w.meta]]
   
   # Now attach true metadata GoogleSheet & reorder columns
   w.meta_v2 <- w.meta_v1 %>% 
-    dplyr::left_join(y = meta_v5, by = c("source", "exp.name")) %>% 
+    dplyr::left_join(y = meta_v6, by = c("source", "exp.name")) %>% 
     dplyr::relocate(exp.design.4:betadisp.comm.dist, 
                     .after = dplyr::everything())
     
@@ -253,10 +256,11 @@ for(focal_w.meta in w.meta_outs){
                                          "excluded.group", "measured.group",
                                          "exp.name", "cage.treatment_std", 
                                          "year", "betadisp.design.level"))
+
+  # Add this to the output list
+  w.meta_out_list[[focal_w.meta]] <- w.meta_v4
   
-  
-  
-}
+} # Close loop
 
 # Check the structure at various points
 ## Starting (no metadata added)
@@ -268,151 +272,30 @@ dplyr::glimpse(w.meta_v3)
 ## After adding summarized beta disp + mean diff
 dplyr::glimpse(w.meta_v4)
 
-
-# BASEMENT----
-
-
-
-# Identify any beta dispersion outputs
-(beta_outs <- dir(path = file.path("data"), pattern = "05-A_caged_beta-disp"))
-
-
-
-# Read in data
-w.meta_v1 <- read.csv(file.path("data", "05_caged_beta-disp_finest-scales.csv"))
-
-# Check structure
-dplyr::glimpse(w.meta_v1)
-
-
-
-
-## ------------------------------------------- ##
-# Join Metadata ----
-## ------------------------------------------- ##
-
-# Actually join the metadata with the 'actual' data
-w.meta_v2 <- w.meta_v1 %>% 
-  dplyr::left_join(y = meta_v5, by = c("source", "exp.name")) %>% 
-  # Relocate all of these columns more intuitively
-  dplyr::relocate(assigned.to:notes, .after = exp.name) %>% 
-  # Drop likely unwanted columns
-  dplyr::select(-assigned.to, -contains("notes"))
-
-# What is lost?
-supportR::diff_check(old = unique(w.meta_v1$source), new = unique(w.meta_v2$source))
-
-# Check structure
-dplyr::glimpse(w.meta_v2)
-
 ## ------------------------------------------- ##
 # Export ----
 ## ------------------------------------------- ##
 
-# Create final object name
-w.meta_v99 <- w.meta_v2
-
-# Identify tidy file name / path
-w.meta_name <- "06_caged_with-metadata_finest-scales.csv"
-w.meta_path <- file.path("data", w.meta_name)
-
-# Export locally
-write.csv(x = w.meta_v99, row.names = F, na = '', file = w.meta_path)
-
-# # Upload to Drive
-# googledrive::drive_upload(media = w.meta_path, overwrite = T,
-#                           path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Acv2ybcpOd_8jEohzgVWcm5qRmgDb4Od"))
-
-## ------------------------------------------- ##
-# Attach Metadata to Design-Specific Beta Disp ----
-## ------------------------------------------- ##
-
-# List exp.design/.name-specific files
-(beta_files <- dir(path = file.path("data"), pattern = "05_caged_beta-disp_exp-"))
-
-# Iterate across them
-for(focal_file in beta_files){
+# Loop across the list elements to export
+for(w.meta_outs in unique(names(w.meta_out_list))){
+  # w.meta_outs <- "05-A_caged_beta-disp_all-scales.csv"
   
-  # Progress message
-  message("Attaching metadata to ", focal_file)
+  # Create a final object
+  w.meta_v99 <- w.meta_out_list[[w.meta_outs]]
   
-  # Read in data
-  focal_df <- read.csv(file = file.path("data", focal_file))
-  
-  # Check for join key mismatches
-  supportR::diff_check(old = unique(focal_df$source), new = unique(meta_v5$source))
-  supportR::diff_check(old = unique(focal_df$exp.name), new = unique(meta_v5$exp.name))
-  
-  # Attach metadata & do minor wrangling
-  focal_join <- focal_df %>% 
-    dplyr::left_join(y = meta_v5, by = c("source", "exp.name")) %>% 
-    dplyr::relocate(assigned.to:notes, .after = exp.name) %>% 
-    dplyr::select(-assigned.to, -notes)
-  
-  # Create a new file name
-  focal_out1 <- gsub(pattern = "beta-disp", replacement = "with-metadata", x = focal_file)
-  focal_out <- gsub(pattern = "05", replacement = "06", x = focal_out1)
+  # Generate tidy name / path
+  w.meta_name <- gsub(pattern = "05-A_caged_beta-disp", 
+                    replacement = "07_caged_w.meta", x = w.meta_outs)
+  w.meta_path <- file.path("data", w.meta_name)
   
   # Export locally
-  write.csv(x = focal_join, na = '', row.names = F,
-            file = file.path("data", focal_out))
+  write.csv(x = w.meta_v99, row.names = F, na = '', file = w.meta_path)
   
 }
 
-# Check the structure of one of those ouputs
-dplyr::glimpse(focal_join)
-
 # # Upload all of these to the Drive
-# purrr::walk(.x = dir(path = file.path("data"), pattern = "06_caged_with-metadata_exp"),
+# purrr::walk(.x = dir(path = file.path("data"), pattern = "07_caged_w.meta"),
 #             .f = ~ googledrive::drive_upload(media = file.path("data", .x), overwrite = T,
 #                                              path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Acv2ybcpOd_8jEohzgVWcm5qRmgDb4Od")))
-
-## ------------------------------------------- ##
-# Attach Metadata to 'All Scales' Beta Disp ----
-## ------------------------------------------- ##
-
-# Read in data
-w.meta_allscales_v1 <- read.csv(file = file.path("data", "05_caged_beta-disp_all-scales.csv"))
-
-# Check for join key mismatches
-supportR::diff_check(old = unique(w.meta_allscales_v1$source), new = unique(meta_v5$source))
-supportR::diff_check(old = unique(w.meta_allscales_v1$exp.name), new = unique(meta_v5$exp.name))
-
-# Attach metadata & do minor wrangling
-w.meta_allscales_v2 <- w.meta_allscales_v1 %>% 
-  dplyr::left_join(y = meta_v5, by = c("source", "exp.name")) %>% 
-  dplyr::relocate(assigned.to:notes, .after = exp.name) %>% 
-  dplyr::select(-assigned.to, -notes)
-
-# Identify tidy file name / path
-w.meta_allscales_name <- "06_caged_with-metadata_all-scales.csv"
-w.meta_allscales_path <- file.path("data", w.meta_allscales_name)
-
-# Export locally
-write.csv(x = w.meta_allscales_v2, row.names = F, na = '', file = w.meta_allscales_path)
-
-# # Upload to Drive
-# googledrive::drive_upload(media = w.meta_allscales_path, overwrite = T,
-#                           path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Acv2ybcpOd_8jEohzgVWcm5qRmgDb4Od"))
-
-
-
-# Create a new file name
-w.meta_allscales_name <- 
-
-focal_out1 <- gsub(pattern = "beta-disp", replacement = "with-metadata", x = focal_file)
-focal_out <- gsub(pattern = "05", replacement = "06", x = focal_out1)
-
-# Export locally
-write.csv(x = focal_join, na = '', row.names = F,
-          file = file.path("data", focal_out))
-# Check structure
-dplyr::glimpse(beta_allscales)
-
-# Export locally
-write.csv(x = beta_allscales, na = '', row.names = F,
-          file = file.path("data", paste0(beta_name, "_all-scales.csv")))
-
-
 
 # End ----
