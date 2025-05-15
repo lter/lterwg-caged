8f24c5ba2843e0069d77bf5866b2289ab0c80dcf
njlyon0 <nicholasjlyon@gmail.com>
2025-05-15 11:45:59 -0400

    feat (expand meta): adding check for mismatch "exp.name" between metadata GoogleSheet and QC data (see lter/scicomp#66)

--- _boatyard_scripts/expand_metadata.R
+++ _boatyard_scripts/expand_metadata.R
@@ -54,7 +54,11 @@ googledrive::drive_download(file = drive_qc$id, overwrite = T,
                             path = file.path("data", drive_qc$name))
 
 # Read in data
-qc_df <- read.csv(file = file.path("data", "02_caged_tidied.csv"))
+qc_df <- read.csv(file = file.path("data", "02_caged_tidied.csv")) %>% 
+  # Keep only needed columns
+  dplyr::select(source, exp.name) %>% 
+  # Drop non-unique rows
+  dplyr::distinct()
 
 # Check structure
 dplyr::glimpse(qc_df)
@@ -66,10 +70,6 @@ dplyr::glimpse(qc_df)
 # Remove data that are in metadata already from QC'd data
 meta_expansion <- qc_df %>% 
   dplyr::filter(!source %in% meta_df$source) %>% 
-  # Drop all but needed columns
-  dplyr::select(source, exp.name) %>% 
-  # Drop non-unique rows
-  dplyr::distinct() %>% 
   # Add needed column(s)
   dplyr::mutate(assigned.to = NA, .after = source)
 
@@ -89,5 +89,31 @@ if(nrow(meta_expansion) != 0){
 ## 3. Open the CSV you just exported above
 ## 4. Copy/paste all of the two columns in that into the end of the data key
 ## 5. Delete the CSV once you've copy/pasted the content into the GoogleSheet
+
+## ------------------------------------------- ##
+# Identify "exp.name" Mismatches ----
+## ------------------------------------------- ##
+
+# Identify files that are in both the data and metadata BUT have diff "exp.name" values
+meta_check <- qc_df %>% 
+  dplyr::filter(source %in% meta_df$source &
+                  !exp.name %in% meta_df$exp.name)
+
+# Re-check structure
+dplyr::glimpse(meta_check)
+
+# Export locally (if any rows need to be added)
+if(nrow(meta_check) != 0){
+  write.csv(x = meta_check, na = '', row.names = F,
+            file = file.path("data", paste0(Sys.Date(), "_exp.name-values-to-check-in-metadata_DELETE-AFTER-USE.csv")))
+}
+
+# NOTE TO PERSON RUNNING CODE:
+## Here's what you should do next:
+## 1. Open the metadata GoogleSheet file
+## 2. Open the CSV you just exported above
+## 3. Check the "exp.name" values in the Sheet that are in the CSV
+### Where possible, update the **GoogleSheet** to match the CSV!
+## 4. Delete the CSV once you've copy/pasted the content into the GoogleSheet
 
 # End ----
