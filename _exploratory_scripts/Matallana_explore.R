ef431ee3cc9676c0bfdf8ea46fd8a21863a67269
nicomatamej <nicomatamej@gmail.com>
2025-05-21 16:52:33 -0600

    Pulled down TerraClimate data, compared with other datasets & began mapping

--- _exploratory_scripts/Matallana_explore.R
+++ _exploratory_scripts/Matallana_explore.R
@@ -8,7 +8,8 @@
 librarian::shelf(tidyverse, googledrive, supportR, raster, ncdf4, 
                  dplyr, sp, ,sf, stringr, magrittr, reshape2, tools, 
                  maps, ggplot2, cowplot, ggspatial, ggrepel,
-                 rnaturalearth, rnaturalearthdata, httpuv, geodata)
+                 rnaturalearth, rnaturalearthdata, httpuv, geodata,
+                 QBMS)
 
 #Set-up google drive connection & import metadata ----
 googledrive::drive_auth(email = "nicomatamej@gmail.com")
@@ -114,6 +115,318 @@ hist(meta.wc.extracts$t.sd.C, main = "Temp S.D. * 100")
 hist(meta.wc.extracts$ppt.mm, main = "Precip. mm") #Some outliers above ~2,000
 hist(meta.wc.extracts[meta.wc.extracts$ppt.mm < 2000,]$ppt.mm, main = "precip < 2000") #precip under 2000mm cutoff
 
+# TerraClim data ----
+
+#Terraclim dataset: https://www.climatologylab.org/terraclimate.html
+#Uses WorldClim & other datasets to interpolate from 1958 up to the present
+
+#prep data
+meta.terr = meta.tidy.simple %>%
+  mutate(ID = 1:length(.$source)) %>%
+  relocate(ID) %>%
+  filter(var_aq.or.terr == "terrestrial")
+
+### tmax
+
+#Prepare for-loop for tmax extraction from Terraclim
+
+var = "tmax"
+
+# enter in variable you want to download see: http://thredds.northwestknowledge.net:8080/thredds/terraclimate_aggregated.html
+baseurlagg <- paste0(paste0("http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_terraclimate_",var),"_1958_CurrentYear_GLOBE.nc")
+
+#tmax
+nc <- nc_open(baseurlagg)
+lon <- ncvar_get(nc, "lon")
+lat <- ncvar_get(nc, "lat")
+
+#ALWAYS RUN THIS BEFORE RE-RUNNING FOR LOOP!!
+tmax.df = data.frame(ID = c(), year = c(), mo = c(), tmax = c()) #create blank dataframe to build in for-loop
+
+#Initialize progress bar
+pb <- txtProgressBar(min = 0, max = length(meta.terr$ID), style = 3)
+
+###For loop to extract monthly average tmax values from 1958 - 2025 at each site.
+#This takes a long time. 169 points to extract from.
+#Make sure to run tmax.df = data.frame(...) above before re-running for-loop!
+
+for (i in 1:length(meta.terr$ID)){ 
+  
+  x = c(meta.terr[i,6], meta.terr[i,5]) #select coordinates
+  
+  #Code to turn coordinates into index for data extraction
+  flat = match(abs(lat - x[2]) < 1/48, 1)
+  latindex = which(flat %in% 1)
+  flon = match(abs(lon - x[1]) < 1/48, 1)
+  lonindex = which(flon %in% 1)
+  start <- c(lonindex, latindex, 1)
+  count <- c(1, 1, -1)
+  
+  
+  # read in the full period of record using aggregated files
+  data = as.numeric(ncvar_get(nc, varid = var,start = start, count))
+  
+  #Create df for each site
+  sub <- data.frame(ID = rep(meta.terr[i,1], times = length(data)),
+                          year = rep(c(1958:2025), each = 12, length.out = length(data)),
+                          mo = rep(1:12, length.out = length(data)),
+                          tmax = data)
+  
+  tmax.df = rbind(tmax.df, sub) #rbind all data together
+  
+  #Print progress
+  setTxtProgressBar(pb, i)
+
+}
+
+### DO NOT RE-RUN: Troubleshoot ID 163
+
+#163: 47.8200000, 13.500000, longitude comes up as NA in lonindex
+#Trying lat/lon 47.82, 13.45 - worked! changed in original google drive sitelevel-metadata file
+
+#Change in current metadata object
+meta.terr$long = if_else(meta.terr$ID == 163, 13.45, meta.terr$long)
+
+#Extract data at new coordinates and rbind to full dataset
+x = c(meta.terr[meta.terr$ID == 163, 6], meta.terr[meta.terr$ID == 163, 5])
+
+#Code to turn coordinates into index for data extraction
+flat = match(abs(lat - x[2]) < 1/48, 1)
+latindex = which(flat %in% 1)
+flon = match(abs(lon - x[1]) < 1/48, 1)
+lonindex = which(flon %in% 1)
+start <- c(lonindex, latindex, 1)
+count <- c(1, 1, -1)
+
+
+# read in the full period of record using aggregated files
+data = as.numeric(ncvar_get(nc, varid = var,start = start, count))
+
+
+#When new coordinate has been fixed & data pulled, add to full dataset
+sub <- data.frame(ID = rep(163, times = length(data)),
+                  year = rep(c(1958:2025), each = 12, length.out = length(data)),
+                  mo = rep(1:12, length.out = length(data)),
+                  tmax = data)
+
+tmax.df = rbind(tmax.df, sub)
+
+
+### DO NOT RE-RUN: Trouble shoot ID 178
+#original coordinates lat/lon: 32.01195, 85.40892
+#test
+x = c(meta.terr[meta.terr$ID == 178, 6], meta.terr[meta.terr$ID == 178, 5])
+
+#Code to turn coordinates into index for data extraction
+flat = match(abs(lat - x[2]) < 1/48, 1)
+latindex = which(flat %in% 1)
+flon = match(abs(lon - x[1]) < 1/48, 1)
+lonindex = which(flon %in% 1)
+start <- c(lonindex, latindex, 1)
+count <- c(1, 1, -1)
+
+# read in the full period of record using aggregated files
+data = as.numeric(ncvar_get(nc, varid = var,start = start, count))
+
+#extraction worked? not sure why for-loop didn't want to work for this guy
+sub <- data.frame(ID = rep(178, times = length(data)),
+                  year = rep(c(1958:2025), each = 12, length.out = length(data)),
+                  mo = rep(1:12, length.out = length(data)),
+                  tmax = data)
+
+tmax.df = rbind(tmax.df, sub) #Added to dataset
+
+hist(tmax.df$tmax)
+
+
+### tmin
+#Prepare for-loop for tmin extraction from Terraclim
+
+var = "tmin"
+
+# enter in variable you want to download see: http://thredds.northwestknowledge.net:8080/thredds/terraclimate_aggregated.html
+baseurlagg <- paste0(paste0("http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_terraclimate_",var),"_1958_CurrentYear_GLOBE.nc")
+
+#tmin
+nc <- nc_open(baseurlagg)
+lon <- ncvar_get(nc, "lon")
+lat <- ncvar_get(nc, "lat")
+
+#Initialize progress bar
+pb <- txtProgressBar(min = 0, max = length(meta.terr$ID), style = 3)
+
+#ALWAYS RUN THIS BEFORE RE-RUNNING FOR LOOP!!
+tmin.df = data.frame(ID = c(), year = c(), mo = c(), tmin = c()) #create blank dataframe to build in for-loop
+
+###For loop to extract monthly average tmmin values from 1958 - 2025 at each site.
+for (i in 1:length(meta.terr$ID)){ 
+  
+  x = c(meta.terr[i,6], meta.terr[i,5]) #select coordinates
+  
+  #Code to turn coordinates into index for data extraction
+  flat = match(abs(lat - x[2]) < 1/48, 1)
+  latindex = which(flat %in% 1)
+  flon = match(abs(lon - x[1]) < 1/48, 1)
+  lonindex = which(flon %in% 1)
+  start <- c(lonindex, latindex, 1)
+  count <- c(1, 1, -1)
+  
+  
+  # read in the full period of record using aggregated files
+  data = as.numeric(ncvar_get(nc, varid = var,start = start, count))
+  
+  #Create df for each site
+  sub = data.frame(ID = rep(meta.terr[i,1], times = length(data)),
+                    year = rep(c(1958:2025), each = 12, length.out = length(data)),
+                    mo = rep(1:12, length.out = length(data)),
+                    tmin = data)
+  
+  tmin.df = rbind(tmin.df, sub) #rbind all data together
+  
+  #Print progress
+  setTxtProgressBar(pb, i)
+  
+}
+
+hist(tmin.df$tmin)
+
+
+###ppt
+#Prepare for-loop for ppt extraction from Terraclim
+
+var = "ppt"
+
+# enter in variable you want to download see: http://thredds.northwestknowledge.net:8080/thredds/terraclimate_aggregated.html
+baseurlagg <- paste0(paste0("http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_terraclimate_",var),"_1958_CurrentYear_GLOBE.nc")
+
+#ppt
+nc <- nc_open(baseurlagg)
+lon <- ncvar_get(nc, "lon")
+lat <- ncvar_get(nc, "lat")
+
+#Initialize progress bar
+pb <- txtProgressBar(min = 0, max = length(meta.terr$ID), style = 3)
+
+#ALWAYS RUN THIS BEFORE RE-RUNNING FOR LOOP!!
+ppt.df = data.frame(ID = c(), year = c(), mo = c(), ppt = c()) #create blank dataframe to build in for-loop
+
+###For loop to extract monthly total ppt values from 1958 - 2025 at each site.
+for (i in 1:length(meta.terr$ID)){ 
+  
+  x = c(meta.terr[i,6], meta.terr[i,5]) #select coordinates
+  
+  #Code to turn coordinates into index for data extraction
+  flat = match(abs(lat - x[2]) < 1/48, 1)
+  latindex = which(flat %in% 1)
+  flon = match(abs(lon - x[1]) < 1/48, 1)
+  lonindex = which(flon %in% 1)
+  start <- c(lonindex, latindex, 1)
+  count <- c(1, 1, -1)
+  
+  
+  # read in the full period of record using aggregated files
+  data = as.numeric(ncvar_get(nc, varid = var,start = start, count))
+  
+  #Create df for each site
+  sub = data.frame(ID = rep(meta.terr[i,1], times = length(data)),
+                   year = rep(c(1958:2025), each = 12, length.out = length(data)),
+                   mo = rep(1:12, length.out = length(data)),
+                   ppt = data)
+  
+  ppt.df = rbind(ppt.df, sub) #rbind all data together
+  
+  #Print progress
+  setTxtProgressBar(pb, i)
+  
+}
+
+
+### DO NOT RE-RUN:Troubleshoot ID 189
+id.189 = meta.worldclim.full %>%
+  filter(ID == 189)
+
+#GPS point is in ocean: try adjusting longitude to put it on land
+#original lat/long: 35.5	-121.1. Trying -121.0, 35.5
+#Worked! Changed lat/long in google drive site.level metadata
+
+#for loop to extract 3 terraclimate variables for ID 189 and add to full data
+
+x = c(-121.0, 35.5) #select coordinates, Long/lat
+
+var = c("tmax", "tmin", "ppt")
+
+#Create DF to cbind data into
+id.189 = list(ID = rep(189, times = 804),
+              year = rep(c(1958:2025), each = 12, length.out = 804),
+              mo = rep(1:12, length.out = 804))
+  
+  data.frame(ID = rep(189, times = 804),
+           year = rep(c(1958:2025), each = 12, length.out = 804),
+           mo = rep(1:12, length.out = 804))
+
+for(i in var){
+  # enter in variable you want to download see: http://thredds.northwestknowledge.net:8080/thredds/terraclimate_aggregated.html
+  baseurlagg <- paste0(paste0("http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_terraclimate_",i),"_1958_CurrentYear_GLOBE.nc")
+  
+  nc <- nc_open(baseurlagg)
+  lon <- ncvar_get(nc, "lon")
+  lat <- ncvar_get(nc, "lat")
+
+  #Code to turn coordinates into index for data extraction
+  flat = match(abs(lat - x[2]) < 1/48, 1)
+  latindex = which(flat %in% 1)
+  flon = match(abs(lon - x[1]) < 1/48, 1)
+  lonindex = which(flon %in% 1)
+  start <- c(lonindex, latindex, 1)
+  count <- c(1, 1, -1)
+
+
+  # read in the full period of record using aggregated files
+  id.189[[i]] = as.numeric(ncvar_get(nc, varid = i,start = start, count))
+  
+  #data = assign(paste(i), as.numeric(ncvar_get(nc, varid = i,start = start, count)))
+
+  #id.189 = cbind(id.189, data)
+
+}
+
+id.189 = as.data.frame(id.189)
+
+
+
+
+#troubleshoot Zero ppt values
+ppt.check = ppt.df %>%
+  filter(ppt == 0) %>%
+  group_by(ID, year) %>%
+  summarise(count = n())
+#No location has a full 12 months of zeros, at most 6 months of zeros
+#proceed with caution
+
+#Combine datasets 
+terraclim.full = tmax.df %>%
+  left_join(tmin.df, by = c("ID", "year", "mo")) %>%
+  left_join(ppt.df, by = c("ID", "year", "mo")) %>%
+  drop_na() %>% #remove old ID 189 rows with NAs
+  rbind(id.189) #rbind new ID 189 rows with data
+
+
+#Calculate aggregated values
+terraclim.avgs = terraclim.full %>%
+  filter(year > 1994) %>% #filter to the last 30 years of the dataset (up until 2024)
+  mutate(tmid = (tmax + tmin)/2) %>% #Average tmax and tmid
+  group_by(ID, year) %>% 
+  summarise(t.avg.yr = mean(tmid), #First find average yearly temperature, within-year s.d. and yearly total ppt
+            t.sd.yr = sd(tmid),
+            ppt.total.yr = sum(ppt)) %>%
+  ungroup() %>%
+  group_by(ID) %>%
+  summarise(t.avg = mean(t.avg.yr), #Total average temperature
+            t.avg.sd = mean(t.sd.yr), #Average yearly standard deviation in temperature
+            ppt.avg = mean(ppt.total.yr)) %>% #average yearly ppt
+  left_join(meta.terr, by = "ID") #Join in basic site metadata
+  
+#
 # Correlations ----
 pairs(meta.wc.extracts[meta.wc.extracts$ppt.mm < 2000,c(4:8)])
 
@@ -147,6 +460,18 @@ summary(npp.temp.tempsd.ppt.lm) #R2 = 0.31
 
 npp.tempsd.lm.ppt.lm = lm(npp.avg ~ ppt.mm + t.sd.C, data = wc.npp.meta) #subtract avg. temp
 summary(npp.tempsd.lm.ppt.lm) #R2 = 0.28 
+
+
+### Check correlations with terraclimate data
+#Join terraclimate with worldclim and npp data
+
+meta.wc.npp.terraclim = wc.npp.meta %>%
+  left_join(terraclim.avgs, by = c("source", "exp.name")) %>%
+  dplyr::select(-ID, -lat.y, -long.y, -var_aq.or.terr.y)
+
+#Visualize correlations
+
+pairs(meta.wc.npp.terraclim[,4:12])
 
 ### Visualize maps ----
 
@@ -228,7 +553,7 @@ wc.map.npp <- ggplot() +
 wc.map.npp
 
 
-# Sites colored by Avg. temp ----
+# Sites colored by WorldClim Avg. temp ----
 wc.map.temp <- ggplot() +
   geom_sf(data = world, fill = "antiquewhite1") +
   coord_sf(xlim = c(-180, 180), ylim = c(-90, 90), expand = FALSE) +
@@ -265,7 +590,7 @@ wc.map.temp
 
 
 
-# Sites colored by yearly temp s.d. ----
+# Sites colored by WorldClim yearly temp s.d. ----
 wc.map.temp.sd <- ggplot() +
   geom_sf(data = world, fill = "antiquewhite1") +
   coord_sf(xlim = c(-180, 180), ylim = c(-90, 90), expand = FALSE) +
@@ -300,7 +625,7 @@ wc.map.temp.sd <- ggplot() +
 
 wc.map.temp.sd
 
-# Sites colored by yearly total precip mm ----
+# Sites colored by WorldClim yearly total precip mm ----
 wc.map.ppt <- ggplot() + #NOTE: high precip outliers removed!
   geom_sf(data = world, fill = "antiquewhite1") +
   coord_sf(xlim = c(-180, 180), ylim = c(-90, 90), expand = FALSE) +
@@ -334,6 +659,46 @@ wc.map.ppt <- ggplot() + #NOTE: high precip outliers removed!
     axis.title = element_text(size = 14, family = "Arial", color = "black"))
 
 wc.map.ppt
+
+# Sites colored by Terraclim vars ----
+
+tc.map <- ggplot() +
+  geom_sf(data = world, fill = "antiquewhite1") +
+  coord_sf(xlim = c(-180, 180), ylim = c(-90, 90), expand = FALSE) +
+  annotation_scale(location = "bl", 
+                   pad_x = unit(0.4, "in"), 
+                   pad_y = unit(0.7, "in"),
+                   height = unit(0.2, "cm"), 
+                   width_hint = 0.2) +
+  annotation_north_arrow(location = "bl", which_north = "true", 
+                         pad_x = unit(0.5, "in"), 
+                         pad_y = unit(0.8, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  geom_point(data = terraclim.avgs, aes(x = long, y = lat, color = t.avg),
+             shape = 19, alpha = 0.5, size = 4) +
+  scale_color_gradientn(colours = rainbow(5), trans = 'reverse') +
+  guides(color=guide_colourbar(bquote(paste("Avg. Temp (C)")))) +
+  #guides(size=guide_legend("Average Yearly Temperature (C)")) +
+  #labs(tag = "A") +
+  xlab("Longitude") + 
+  ylab("Latitude") +
+  theme(#legend.position = "none",
+    legend.background = element_blank(),
+    legend.box.background = element_blank(),
+    legend.key = element_blank(),
+    legend.text = element_text(size = 10, family = "Arial", color = "black"),
+    panel.grid.major = element_line(colour = gray(0.5), linetype = "dashed", size = 0.2), 
+    panel.background = element_rect(fill = "aliceblue"), 
+    panel.border = element_rect(fill = NA),
+    axis.text.y = element_text(size = 12, family = "Arial", color = "black"),
+    axis.text.x = element_text(size = 12, family = "Arial", color = "black"),
+    axis.title = element_text(size = 14, family = "Arial", color = "black"))
+
+tc.map
+
+
+
+
 
 # Next steps ----
 
