a4b1bbe29ed0ec1e1ddff22e1fbfceea9f364fa4
Max Castorani <8293602+maxcastorani@users.noreply.github.com>
2025-06-09 16:46:26 -0400

    Beta regression modeling and output

--- 08c_stats.R
+++ 08c_stats.R
@@ -8,8 +8,8 @@
 ## ------------------------------------------- ##
 
 # Load libraries (performance might be within easystats)
-librarian::shelf(tidyverse, ltertools, lme4, lmerTest, 
-                 performance, easystats, lubridate, car, 
+librarian::shelf(tidyverse, ltertools, lme4, lmerTest, glmmTMB, DHARMa,
+                 performance, easystats, lubridate, car, broom.mixed,
                  njlyon0/supportR, MuMIn, visreg, 
                  emmeans, tidymodels, qqplotr, sjPlot) #, update_all= TRUE) 
 
@@ -39,17 +39,334 @@ BaeDisp.df <- read.csv("data/BaeDisp.df.csv")
 ## ------------------------------------------- ##
 
 glimpse(BaeDisp.df) #10,881 rows
-hist(BaeDisp.df$betadisp.comm.dist)
-range(BaeDisp.df$betadisp.comm.dist)
+
+# Check distribution of values in response variable, betadisp.comm.dist
+hist(BaeDisp.df$betadisp.comm.dist)  
+
+# Check range of values in response variable, betadisp.comm.dist, with increased precision
+options(digits = 22) # 22 is maximum value allowable
+range(BaeDisp.df$betadisp.comm.dist) 
+
+# Check relative frequencies of true ones
+table(BaeDisp.df$betadisp.comm.dist == 1)
+
+# Check relative frequencies of true zeroes
+table(BaeDisp.df$betadisp.comm.dist == 0) 
+paste0("Frequency of true zeroes is ",
+       round(100 * table(BaeDisp.df$betadisp.comm.dist == 0)[2] / nrow(BaeDisp.df), 2), 
+       "%"
+       )
+
+# Reset number of significant digits to the default value (7)
+options(digits = 7)
+
+# Due to the possible presence of true zeroes and ones, which are invalid in beta regression (i.e., we have [0,1] but the model assumes (0,1)), we transform values slightly inward such that they are above 0 and below 1 by a small amount, epsilon. There is a boundary distortion, but there are sound reasons for taking this approach:
+# (1) There are no true ones, but some true zeroes
+# (2) True zeroes are rare, representing 76 / 10,618 = less than 1% of the data
+# (3) True zeroes and ones are not structurally different from values 0 < y < 1
+# (4) Model results are readily interpretable and process is simple to understand and implement
+# (4) Zero-inflated beta model could be considered, but this would require fitting separate binary models for the probability of zeroes and ones, which does not make sense in our application as again the process generating true zeroes and ones should be the same process as that generating 0 < y < 1
+# Reference: Smithson & Verkuilen (2006), "A better lemon squeezer? Maximum-likelihood regression with beta-distributed dependent variables."
+
+# Data are in the semi-closed interval [0,1), i.e., they include true 0s but no 1s. Hence, we apply a transformation that only adjusts the zeroes while leaving the interior values nearly unchanged
+# This nudges zeros to a small positive value but barely affects values near 1
+n <- nrow(BaeDisp.df)
+transform.fun <- function(y){(y * (n - 1) + 0.5 ) / n}
+
+# Alternative transformation if you have true zeroes and true ones, [0, 1] - DO NOT NEED TO USE
+#transform.fun <- function(y){y * (1 - epsilon) + (epsilon / 2)}
+#epsilon <- 1e-6
+
+# Apply transformation
+BaeDisp.df$betadisp.comm.dist_transform <- sapply(X = BaeDisp.df$betadisp.comm.dist, FUN = transform.fun)
+
+# Validate (this should look like a 1:1)
+plot(x = BaeDisp.df$betadisp.comm.dist, y =  BaeDisp.df$betadisp.comm.dist_transform)
+
+
+## ------------------------------------------- ##
+## Create new dataframes for models to follow ----
+## ------------------------------------------- ##
+# Drop rows for which there is no terrestrial or aquatic categorization
+table(BaeDisp.df$var_aq.or.terr)
+
+BaeDisp.df2 <- BaeDisp.df %>% 
+  dplyr::filter(var_aq.or.terr != "") %>% 
+  droplevels()
+
+table(BaeDisp.df2$var_aq.or.terr)
+
+# Drop rows for which latitude is missing
+table(is.na(BaeDisp.df2$lat))
+
+BaeDisp.df2 <- BaeDisp.df2[!is.na(BaeDisp.df2$lat), ]
+
+table(is.na(BaeDisp.df2$lat))
+
+# Create a column for absolute value of latitude
+BaeDisp.df2 <- BaeDisp.df2 %>%
+  mutate(abs.lat = abs(lat))
+
+# Make sure there are no NAs for the experiment name
+table(is.na(BaeDisp.df2$exp.name))
+
+# Make aquatic/terrestrial a factor
+BaeDisp.df2$var_aq.or.terr <- factor(BaeDisp.df2$var_aq.or.terr)
+
+# Subset datasets for uncaged plots only or caged plots only
+uncaged.df <- BaeDisp.df2 %>%
+  filter(cage.treatment_std == "uncaged") %>%
+  droplevels()
+
+caged.df <- BaeDisp.df2 %>%
+  filter(cage.treatment_std == "caged") %>%
+  droplevels()
+
+# Check data: Collinearity between x1 and x2?
+ggplot(data = uncaged.df, aes(x = var_aq.or.terr, y = abs.lat)) +
+  geom_boxplot()
+
+ggplot(data = caged.df, aes(x = var_aq.or.terr, y = abs.lat)) +
+  geom_boxplot()
+
+# Check data: General relationships between y and x1 or x2
+ggplot(data = BaeDisp.df2, aes(x = lat, y = betadisp.comm.dist_transform)) +
+  geom_point() +
+  geom_smooth(method = 'lm', formula = y ~ poly(x, 2)) +
+  facet_grid(cage.treatment_std ~ var_aq.or.terr) 
+ggplot(data = BaeDisp.df2, aes(x = cage.treatment_std, y = betadisp.comm.dist_transform)) +
+  geom_boxplot()
+ggplot(data = BaeDisp.df2, aes(x = var_aq.or.terr, y = betadisp.comm.dist_transform)) +
+  geom_boxplot()
+ggplot(data = BaeDisp.df2, aes(x = cage.treatment_std, y = betadisp.comm.dist_transform)) +
+  geom_boxplot() +
+  facet_wrap( ~ var_aq.or.terr) 
+
+ggplot(data = uncaged.df, aes(x = lat, y = betadisp.comm.dist_transform)) +
+  geom_point() +
+  geom_smooth(method = 'lm', formula = y ~ poly(x, 2)) 
+ggplot(data = uncaged.df, aes(x = abs.lat, y = betadisp.comm.dist_transform)) +
+  geom_point() +
+  geom_smooth(method = 'lm') 
+ggplot(data = uncaged.df, aes(x = var_aq.or.terr, y = betadisp.comm.dist_transform)) +
+  geom_boxplot()
+
+ggplot(data = uncaged.df, aes(x = lat, y = betadisp.comm.dist_transform)) +
+  geom_point() +
+  geom_smooth(method = 'lm', formula = y ~ poly(x, 2)) +
+  facet_wrap(~var_aq.or.terr)
+
+ggplot(data = caged.df, aes(x = lat, y = betadisp.comm.dist_transform)) +
+  geom_point() +
+  geom_smooth(method = 'lm', formula = y ~ poly(x, 2)) 
+ggplot(data = caged.df, aes(x = abs.lat, y = betadisp.comm.dist_transform)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+ggplot(data = caged.df, aes(x = var_aq.or.terr, y = betadisp.comm.dist_transform)) +
+  geom_boxplot()
+
+
+## ------------------------------------------- ##
+## Fit Beta Regression Models ----
+## ------------------------------------------- ##
+
+# The scatterplots above suggested that trends using an absolute value of latitude might yield different results than latitude. This is perhaps because of the distribution of studies along the latitudinal gradient.
+
+# Hence modeling latitude using a parabolic function (y ~ poly(x, 2)). Tried these models using an absolute latitude and found the same result (no change in significance)
+
+# Nesting within source is generally preferable because it better reflects study-level heterogeneity.
+# (1 | exp.name) assumes all experiments are independent, even if they come from the same paper. This underestimates variance and overstates precision, inflating test statistics, treating intra-paper correlation as noise or ignoring it.
+# (1 | source / exp.name) models the hierarchical structure in which experiments within a paper are not fully independent. This increases the estimated uncertainty (increasing p-values vs. not including source), especially if some papers contribute many experiments. 
+# Hence, using random effects as "(1|source/exp.name)"
+
+# In preliminiary model fitting, DHARMa indicated a bit of problem with dispersion, so adding a dispersion term to model not only the mean response but the variance of the response. Essentially this is allowing the dispersion to differ between aquatic and terrestrial studies, and with latitude. This is pretty apparent from the exploratory plots above, too.
+
+# Fit model 1 on all data
+uncaged.betamod1 <- glmmTMB(betadisp.comm.dist_transform ~ 
+                              cage.treatment_std +
+                              var_aq.or.terr     +
+                              poly(lat, 2)       +
+                              cage.treatment_std * var_aq.or.terr +
+                              cage.treatment_std * poly(lat, 2)   +
+                              var_aq.or.terr     * poly(lat, 2)   +
+                              cage.treatment_std * var_aq.or.terr * poly(lat, 2) + 
+                              (1|source/exp.name), 
+                            dispformula = ~ var_aq.or.terr + poly(lat, 2),
+                            family = beta_family(link = "logit"),
+                            control = glmmTMBControl(optimizer = optim, 
+                                                     optArgs = list(method = "BFGS")), 
+                            # Or use nlminb, or bobyqa via nloptr
+                            data = BaeDisp.df2) 
+summary(uncaged.betamod1)    
+car::Anova(uncaged.betamod1, type = "II")
+
+# Drop 3-way interaction
+uncaged.betamod1 <- glmmTMB(betadisp.comm.dist_transform ~ 
+                              cage.treatment_std +
+                              var_aq.or.terr     +
+                              poly(lat, 2)       +
+                              cage.treatment_std * var_aq.or.terr +
+                              cage.treatment_std * poly(lat, 2)   +
+                              var_aq.or.terr     * poly(lat, 2)   +
+                              (1|source/exp.name), 
+                            dispformula = ~ var_aq.or.terr + poly(lat, 2),
+                            family = beta_family(link = "logit"),
+                            control = glmmTMBControl(optimizer = optim, 
+                                                     optArgs = list(method = "BFGS")), 
+                            # Or use nlminb, or bobyqa via nloptr
+                            data = BaeDisp.df2) 
+summary(uncaged.betamod1)    
+car::Anova(uncaged.betamod1, type = "II")
+
+# Drop 2-way interaction of cage treatment * aquatic/terrestrial
+uncaged.betamod1 <- glmmTMB(betadisp.comm.dist_transform ~ 
+                              cage.treatment_std +
+                              var_aq.or.terr     +
+                              poly(lat, 2)       +
+                              cage.treatment_std * poly(lat, 2)   +
+                              var_aq.or.terr     * poly(lat, 2)   +
+                              (1|source/exp.name), 
+                            dispformula = ~ var_aq.or.terr + poly(lat, 2),
+                            family = beta_family(link = "logit"),
+                            control = glmmTMBControl(optimizer = optim, 
+                                                     optArgs = list(method = "BFGS")), 
+                            # Or use nlminb, or bobyqa via nloptr
+                            data = BaeDisp.df2) 
+summary(uncaged.betamod1)    
+car::Anova(uncaged.betamod1, type = "III")
+
+# Summarize model output and statistics
+tidy(Anova(uncaged.betamod1, type = "II"))
+
+## ------------------------------------------- ##
+## Validate Beta Regression Models ----
+## ------------------------------------------- ##
+
+# Validate with DHARMa
+dat <- BaeDisp.df2
+fittedModel <- uncaged.betamod1
+
+simulationOutput <- simulateResiduals(fittedModel = fittedModel, plot = F)
+
+plot(simulationOutput)
+# Note that there is "significant" deviation, but it is easy to get because n > 9,000!
+# The deviation does not look troubling in its magnitude
+
+# In addition to plotting the residuals vs. fitted, we must plot residuals vs. individual predictors
+plotResiduals(simulationOutput, form = dat$cage.treatment_std)
+plotResiduals(simulationOutput, form = dat$var_aq.or.terr)
+plotResiduals(simulationOutput, form = dat$abs.lat)
+
+testDispersion(simulationOutput)
+# Dispersion looks fine
+
+## ------------------------------------------- ##
+## Generate Predictions for Beta Regression Models ----
+## ------------------------------------------- ##
+
+# Save original polynomial transformation
+lat_poly <- poly(BaeDisp.df2$lat, 2)
+
+# Generate prediction grid
+newdata <- expand.grid(
+  cage.treatment_std = unique(BaeDisp.df2$cage.treatment_std),
+  var_aq.or.terr     = unique(BaeDisp.df2$var_aq.or.terr),
+  lat                = seq(min(BaeDisp.df2$lat), max(BaeDisp.df2$lat), length.out = 100)
+)
+
+# Create poly terms using same basis
+poly_lat_new <- predict(lat_poly, newdata$lat)
+newdata$`poly(lat, 2)1` <- poly_lat_new[, 1]
+newdata$`poly(lat, 2)2` <- poly_lat_new[, 2]
+
+# Dummy grouping vars for prediction
+newdata$source <- NA
+newdata$exp.name <- NA
+
+# Get predictions with SE
+preds <- predict(uncaged.betamod1, newdata = newdata, type = "response", se.fit = TRUE)
+newdata$fit   <- preds$fit
+newdata$se    <- preds$se.fit
+newdata$lower <- newdata$fit - 1.96 * newdata$se
+newdata$upper <- newdata$fit + 1.96 * newdata$se
+
+# Plot: points = raw data; line = prediction; ribbon = 95% CI; facet = two key interactions
+
+y_colors <- c("aquatic" = "#1f78b4",      # blue
+              "terrestrial" = "#33a02c")  # green
+
+my_shapes <- c("caged" = 16,    # solid circle
+               "uncaged" = 17)  # solid triangle
+
+ggplot(BaeDisp.df2, aes(x = lat, y = betadisp.comm.dist_transform)) +
+  geom_point(aes(color = var_aq.or.terr, shape = cage.treatment_std), alpha = 0.4) +
+  geom_line(data = newdata, aes(y = fit, color = var_aq.or.terr), size = 1) +
+  geom_ribbon(data = newdata, 
+              aes(x = lat, ymin = lower, ymax = upper, fill = var_aq.or.terr), 
+              alpha = 0.2, inherit.aes = FALSE) +
+  facet_grid(cage.treatment_std ~ var_aq.or.terr) +
+  labs(y = "Beta Dispersion Distance (± 95% CI)",
+       x = "Latitude",
+       title = "Raw data with marginal model predictions by treatment and ecosystem type") +
+  theme_classic() +
+  theme(panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),
+        aspect.ratio = 1) +
+  scale_color_manual(values = my_colors) +
+  scale_fill_manual(values = my_colors) +
+  scale_shape_manual(values = my_shapes)
+ggsave("graphs/beta.reg.output1-raw.data.pdf", height = 8, width = 8)
+
+# Compute study-level means
+study_means <- BaeDisp.df2 %>%
+  group_by(source, cage.treatment_std, var_aq.or.terr) %>%
+  summarise(
+    lat  = mean(lat),
+    mean_response = mean(betadisp.comm.dist_transform),
+    se_response   = sd(betadisp.comm.dist_transform) / sqrt(n()),
+    .groups = "drop"
+  ) %>%
+  mutate(
+    lower_ci = mean_response - 1.96 * se_response,
+    upper_ci = mean_response + 1.96 * se_response
+  )
+
+# Use study-level points instead of raw experiment-level points
+ggplot(study_means, aes(x = lat, y = mean_response)) +
+  geom_point(aes(color = var_aq.or.terr, shape = cage.treatment_std), size = 2.5, alpha = 0.7) +
+  geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci, color = var_aq.or.terr), width = 0.4) +
+  geom_line(data = newdata,
+            aes(x = lat, y = fit, color = var_aq.or.terr, group = cage.treatment_std), 
+            size = 1) +
+  geom_ribbon(data = newdata, 
+              aes(x = lat, ymin = lower, ymax = upper, fill = var_aq.or.terr), 
+              alpha = 0.2, inherit.aes = FALSE) +
+  facet_grid(cage.treatment_std ~ var_aq.or.terr) +
+  labs(y = "Beta Dispersion Distance (± 95% CI)",
+       x = "Latitude",
+       title = "Study-level mean data with marginal model predictions by treatment and ecosystem type") +
+  theme_classic() +
+  theme(
+    panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),
+    aspect.ratio = 1
+  ) +
+  scale_color_manual(values = my_colors) +
+  scale_fill_manual(values = my_colors) +
+  scale_shape_manual(values = my_shapes)
+ggsave("graphs/beta.reg.output2-study.means.pdf", height = 8, width = 8)
+
+## ------------------------------------------- ##
+## ------------------------------------------- ##
+# Max stopped working here 2025-06-09
+## ------------------------------------------- ##
+## ------------------------------------------- ##
+
+
+
 
 ## ------------------------------------------- ##
 ## Figure 2 ----
 ## ------------------------------------------- ##
 # lmer for uncaged only (Figure 2 model)
-uncaged.df <- BaeDisp.df %>%
-  filter(cage.treatment_std == "uncaged") %>%
-  mutate(abs.lat = abs(lat))
-
 uncaged.mod1 <- lmer(betadisp.comm.dist ~ 
                            var_aq.or.terr*abs.lat + 
                            (1|exp.name), data = uncaged.df)
@@ -66,10 +383,6 @@ plot_model(uncaged.mod1)
 
 
 # lmer for caged only (Supplement to figure 2)
-caged.df <- BaeDisp.df %>%
-  filter(cage.treatment_std == "caged")%>%
-  mutate(abs.lat = abs(lat))
-
 caged.mod1 <- lmer(betadisp.comm.dist ~ 
                        var_aq.or.terr*abs.lat + 
                        (1|exp.name), data = caged.df)
