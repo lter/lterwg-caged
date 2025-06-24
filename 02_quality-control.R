cd52657e44696eede870b237bcc8179d41ed1f11
njlyon0 <nicholasjlyon@gmail.com>
2025-06-24 10:35:01 -0400

    fix (02-04): correcting which packages are loaded for these scripts

--- 02_quality-control.R
+++ 02_quality-control.R
@@ -8,7 +8,7 @@
 ## ------------------------------------------- ##
 
 # Load libraries
-librarian::shelf(tidyverse, ltertools)
+librarian::shelf(tidyverse, supportR)
 
 # Create needed folder(s)
 dir.create(path = file.path("data"), showWarnings = F)
