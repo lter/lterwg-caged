991ec12fba6dc73adc012b7f74c3a8214856acfc
njlyon0 <nicholasjlyon@gmail.com>
2025-09-10 14:53:06 -0400

    feat (diagnostic): wrote a catch-all 'where is file X dropped in our workflow' script (#25)

--- _exploratory_scripts/diagnose_dropped-sources.R
+++ _exploratory_scripts/diagnose_dropped-sources.R
@@ -8,7 +8,7 @@
 ## ------------------------------------------- ##
 
 # Load libraries
-librarian::shelf(tidyverse)
+librarian::shelf(tidyverse, ltertools)
 
 # Create needed folder(s)
 dir.create(path = file.path("data", "diagnostic"), showWarnings = F, recursive = T)
@@ -17,67 +17,70 @@ dir.create(path = file.path("data", "diagnostic"), showWarnings = F, recursive =
 rm(list = ls()); gc()
 
 # Manually identify files of interest
-want_src <- c("gilson_southafrica_intertidalexclusion_2021_grazers_algae.csv",
-              "porensky_wyoming_nex_2015-2024_prairiedogs_vegetation.csv",
-              "parker_wetlands_carpgrass_2005_crayfish_plants.csv",
-              "lamb_galapagos_consumermobility_2017_fish-urchins_algae.csv", # (exp.name = camano protected warm)
-              "lter-sevilleta_newmexico_sev-project_1995-2005_smallmammals_vegetation.csv",
-              "gex_beevermojave_mojave_2002_burrows&cattle_plants.csv",
-              "ashton_coastalamerica_marinepredexcl_2017-2019_predators_benthic.csv" #(exp.name = COL)
-)
+want_src <- c("parker_wetlands_carpgrass_2005_crayfish_plants.csv")
 
 ## ------------------------------------------- ##
-# Load Data ----
+# Load All Tidy Data ----
 ## ------------------------------------------- ##
 
-# Read in the beta dispersion values
-beta_v1 <- read.csv(file = file.path("data", "05-A_caged_beta-disp_finest-scales.csv"))
+# Get a list of data objects
+diag_v1 <- ltertools::read(raw_folder = file.path("data"), data_format = "csv") %>% 
+  # Pare down columns
+  purrr::map(.x = ., .f = ~ dplyr::select(
+    .data = .x, source, exp.name, dplyr::starts_with(c("exp.design.", "cage.treatment_std"))
+  )) %>% 
+  # Drop some columns specifically
+  purrr::map(.x = ., .f = ~ dplyr::select(
+    .data = .x, -dplyr::contains("spatialextent.category"))) %>% 
+  # Drop non-unique rows
+  purrr::map(.x = ., .f = dplyr::distinct) %>% 
+  # Get 'tidy source' as a column in its own right
+  purrr::imap(.x = ., 
+              .f = ~ dplyr::mutate(.data = .x, tidy_source = .y, 
+                                   .before = source)) %>% 
+  # Unlist to dataframe
+  purrr::list_rbind(x = .) %>% 
+  # Ditch the very first harmonized data and 
+  ## (01 is not super useful as a diagnostic because it hasn't had any QC)
+  ## (gamma richness is not useful because it doesn't have treatment info)
+  dplyr::filter(!tidy_source %in% c("01_caged_harmonized.csv", "05-B_caged_gamma-rich.csv"))
 
 # Check structure
-dplyr::glimpse(beta_v1)
-
-# Read in the final pre-stats data too
-pre.stats_v1 <- read.csv(file = file.path("data", "07_caged_w.meta_finest-scales.csv"))
-
-# Check structure
-dplyr::glimpse(pre.stats_v1)
+dplyr::glimpse(diag_v1)
 
 ## ------------------------------------------- ##
-# Prepare Data ----
+# Diagnose Data ----
 ## ------------------------------------------- ##
 
-# Streamline beta dispersion df to just needed info for diagnostics
-beta_v2 <- beta_v1 %>% 
-  # Immediately filter to desired sources
-  dplyr::filter(source %in% want_src) %>% 
-  dplyr::filter(source != "lamb_galapagos_consumermobility_2017_fish-urchins_algae.csv" |
-                  (source == "lamb_galapagos_consumermobility_2017_fish-urchins_algae.csv" & exp.name == "Camano_protected-Warm")) %>% 
-  dplyr::filter(source != "ashton_coastalamerica_marinepredexcl_2017-2019_predators_benthic.csv" |
-                  (source == "ashton_coastalamerica_marinepredexcl_2017-2019_predators_benthic.csv" & exp.name == "COL")) %>% 
-  # Pare down to just needed columns
-  dplyr::select(source, dplyr::starts_with("exp."), cage.treatment_std, year,
-                dplyr::starts_with("betadisp")) %>% 
-  # Pare down to unique rows
-  dplyr::distinct()
-
-# Check structure
-dplyr::glimpse(beta_v2)
-
-# Process the other df too
-pre.stats_v2 <- pre.stats_v1  %>% 
-  # Immediately filter to desired sources
-  dplyr::filter(source %in% want_src) %>% 
-  dplyr::filter(source != "lamb_galapagos_consumermobility_2017_fish-urchins_algae.csv" |
-                  (source == "lamb_galapagos_consumermobility_2017_fish-urchins_algae.csv" & exp.name == "Camano_protected-Warm")) %>% 
-  dplyr::filter(source != "ashton_coastalamerica_marinepredexcl_2017-2019_predators_benthic.csv" |
-                  (source == "ashton_coastalamerica_marinepredexcl_2017-2019_predators_benthic.csv" & exp.name == "COL")) %>% 
-  # Pare down to just needed columns
-  dplyr::select(source, dplyr::starts_with("exp."), cage.treatment_std, year,
-                dplyr::starts_with(c("betadisp", "within.cage.treat"))) %>% 
-  # Pare down to unique rows
-  dplyr::distinct()
-
-# Check structure
-dplyr::glimpse(pre.stats_v2)
+# Loop across provided source files
+for(focal_src in sort(unique(want_src))){
+  
+  # Progress message
+  message("Diagnosing dataset: '", focal_src, "'")
+  
+  # Subset the 'all tidy data' to just this dataset
+  diag_v2 <- diag_v1 %>% 
+    dplyr::filter(source == focal_src)
+  
+  # Now actually generate the diagnostic output
+  diag_v3 <- diag_v2 %>% 
+    # Pare down columns/rows (again)
+    dplyr::select(tidy_source, source, exp.name, cage.treatment_std) %>% 
+    dplyr::distinct() %>% 
+    # Wrangle the tidy source column slightly
+    dplyr::mutate(tidy_source = gsub(pattern = "-", replacement = ".", x = tidy_source)) %>% 
+    # Make a placeholder column (we'll need it in a sec)
+    dplyr::mutate(presence = "present") %>% 
+    # Identify whether the dataset is in each of the remaining tidy sources
+    tidyr::pivot_wider(names_from = tidy_source, values_from = presence, values_fill = "MISSING")
+  
+  # Generate a nice-ish file name for this diagnostic output
+  diag_name <- paste0("workflow-drop-diagnostic_", focal_src)
+  
+  # Export this locally
+  write.csv(x = diag_v3, row.names = F, na = '',
+            file = file.path("data", "diagnostic", diag_name))
+  
+} # Close loop
 
 # End ----
