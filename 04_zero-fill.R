b3599d0f25e67dafc71cd4591cdfa275dde2bb42
njlyon0 <nicholasjlyon@gmail.com>
2025-09-15 11:43:05 -0400

    refactor (zero fill): replacing folder creation with call to dedicated 'setup' script

--- 04_zero-fill.R
+++ 04_zero-fill.R
@@ -15,8 +15,8 @@
 # Load libraries
 librarian::shelf(tidyverse, supportR)
 
-# Create needed folder(s)
-dir.create(path = file.path("data"), showWarnings = F)
+# Create needed folders
+source(file = file.path("00_setup.R"))
 
 # Clear environment + collect garbage
 rm(list = ls()); gc()
@@ -78,7 +78,7 @@ lost_rows <- fill_v1 %>%
   dplyr::ungroup() %>%
   # Filter to only the datasets where we are losing rows
   dplyr::filter(row.count > 1)
-  
+
 # Check that out
 dplyr::glimpse(lost_rows)
 ## View(lost_rows)
@@ -158,7 +158,7 @@ zerofill_path <- file.path("data", zerofill_name)
 write.csv(x = fill_v99, row.names = F, na = '', file = zerofill_path)
 
 # # Upload to Drive
-#googledrive::drive_upload(media = zerofill_path, overwrite = T,
- #                         path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Acv2ybcpOd_8jEohzgVWcm5qRmgDb4Od"))
+# googledrive::drive_upload(media = zerofill_path, overwrite = T,
+#                         path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Acv2ybcpOd_8jEohzgVWcm5qRmgDb4Od"))
 
 # End ----
