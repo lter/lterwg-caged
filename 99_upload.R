f2c0efc2ddd199880d0c70d088db4097d3607ecc
njlyon0 <nicholasjlyon@gmail.com>
2025-09-18 12:08:38 -0400

    feat (upload): commenting out files that are giant with a note to that effect

--- 99_upload.R
+++ 99_upload.R
@@ -23,20 +23,24 @@ rm(list = ls()); gc()
 drive_tidy <- googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Acv2ybcpOd_8jEohzgVWcm5qRmgDb4Od")
 
 # Upload harmonized data
-googledrive::drive_upload(media = file.path("data", "01_caged_harmonized.csv"),
-                          overwrite = T, path = drive_tidy)
+## As of 9/18/25 this file is >500 MB so better to upload manually
+# googledrive::drive_upload(media = file.path("data", "01_caged_harmonized.csv"),
+#                           overwrite = T, path = drive_tidy)
 
 # Upload QC'd data
-googledrive::drive_upload(media = file.path("data", "02_caged_tidied.csv"),
-                          overwrite = T, path = drive_tidy)
+## As of 9/18/25 this file is nearly 1 GB so better to upload manually
+# googledrive::drive_upload(media = file.path("data", "02_caged_tidied.csv"),
+#                           overwrite = T, path = drive_tidy)
 
 # Upload filtered data
-googledrive::drive_upload(media = file.path("data", "03_caged_filtered.csv"),
-                          overwrite = T, path = drive_tidy)
+## As of 9/18/25 this file is ~300 MB so better to upload manually
+# googledrive::drive_upload(media = file.path("data", "03_caged_filtered.csv"),
+#                           overwrite = T, path = drive_tidy)
 
 # Upload zero-filled data
-googledrive::drive_upload(media = file.path("data", "04_caged_zero-filled.csv"),
-                          overwrite = T, path = drive_tidy)
+## As of 9/18/25 this file is ~300 MB so better to upload manually
+# googledrive::drive_upload(media = file.path("data", "04_caged_zero-filled.csv"),
+#                           overwrite = T, path = drive_tidy)
 
 # Upload 'all scales' and 'finest scales' beta dispersion
 purrr::walk(.x = dir(path = file.path("data"), pattern = "05-A_caged_beta-disp"),
