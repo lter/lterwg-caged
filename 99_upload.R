6eb03d6136c696e1211f2e0e93989ef82fe87785
njlyon0 <nicholasjlyon@gmail.com>
2025-10-14 15:49:49 -0400

    fix (upload): updating `08` file name slug at upload

--- 99_upload.R
+++ 99_upload.R
@@ -62,7 +62,7 @@ purrr::walk(.x = dir(path = file.path("data"), pattern = "07_caged_w.meta"),
                                              overwrite = T, path = drive_tidy))
 
 # Upload data tables prepared for statistics
-purrr::walk(.x = dir(path = file.path("data"), pattern = "08a_caged_prepped-"),
+purrr::walk(.x = dir(path = file.path("data"), pattern = "08_caged_prepped-"),
             .f = ~ googledrive::drive_upload(media = file.path("data", .x), 
                                              overwrite = T, path = drive_tidy))
 # End ----
