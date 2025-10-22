2ecf3587dc5bdc21ed22154ff7e72a61d69e5eb3
Jamie McDevitt-Irwin <jamie.mcirwin@gmail.com>
2025-10-22 10:49:45 -1000

    updating number of rows

--- 09a_raw-data-figs.R
+++ 09a_raw-data-figs.R
@@ -27,8 +27,8 @@ caged_beta <- read.csv(file.path("data", "08_caged_prepped-beta-dispersion.csv")
 
 # View(caged_effectsize) 
 # View(caged_beta) 
-dim(caged_effectsize)# 273 rows
-dim(caged_beta)# 11281 rows
+dim(caged_effectsize)# 365 rows
+dim(caged_beta)# 12235 rows
 
 ## ------------------------------------------- ##
 # Plots ---- 
