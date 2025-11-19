f53eb47a0d122efd421661c4d79e889a4f8fa298
njlyon0 <nicholasjlyon@gmail.com>
2025-11-19 09:56:16 -0500

    feat (beta dev): loading likely-needed data

--- 06-B_calc-beta-deviation.R
+++ 06-B_calc-beta-deviation.R
@@ -19,7 +19,23 @@ rm(list = ls()); gc()
 # Load Data ----
 ## ------------------------------------------- ##
 
+# Load beta dispersion data
+dsp_v01 <- read.csv(file = file.path("data", "05-A_caged_beta-disp_all-scales.csv"))
 
+# Check structure
+dplyr::glimpse(dsp_v01)
+
+# Also load gamma richness
+gam_v01 <- read.csv(file = file.path("data", "05-B_caged_gamma-rich.csv"))
+
+# Check structure
+dplyr::glimpse(gam_v01)
+
+# Also load pre-beta dispersion (for abundance values)
+abn_v01 <- read.csv(file = file.path("data", "04_caged_zero-filled.csv"))
+
+# Check structure
+dplyr::glimpse(abn_v01)
 
 
 # End ----
