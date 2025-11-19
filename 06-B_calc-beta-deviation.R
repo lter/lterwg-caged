53a19bdad1baa7c7cd01e5ec6cd5af9f948f9aba
njlyon0 <nicholasjlyon@gmail.com>
2025-11-19 08:50:07 -0500

    feat (beta dev): start of script for deviation calc (#33)

---
+++ 06-B_calc-beta-deviation.R
@@ -0,0 +1,25 @@
+## --------------------------------------------------------------- ##
+# CAGED *Mean* Difference in Beta Dispersion Calculation
+## --------------------------------------------------------------- ##
+
+## ------------------------------------------- ##
+# Housekeeping ----
+## ------------------------------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse, supportR)
+
+# Create needed folders
+source(file = file.path("00_setup.R"))
+
+# Clear environment + collect garbage
+rm(list = ls()); gc()
+
+## ------------------------------------------- ##
+# Load Data ----
+## ------------------------------------------- ##
+
+
+
+
+# End ----
