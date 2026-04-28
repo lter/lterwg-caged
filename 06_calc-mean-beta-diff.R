01a06fc903e04e6768ddcb507eb3c5adc9465427
Jamie McDevitt-Irwin <jamie.mcirwin@gmail.com>
2026-04-27 15:39:04 -1000

    updating 07 sources/experiments after alderson is dropped

--- 06_calc-mean-beta-diff.R
+++ 06_calc-mean-beta-diff.R
@@ -117,6 +117,6 @@ for(diff_outs in unique(names(diff_list))){
 
 # End ----
 
-unique(diff_v99$source) # 118
-unique(diff_v99$exp.name) # 348
+unique(diff_v99$source) # 117
+unique(diff_v99$exp.name) # 346
 
