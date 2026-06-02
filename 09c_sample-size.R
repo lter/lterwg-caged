6e96a49143686b894f4cb9d2e8972209097ba572
hillarykrumbholz <hkrumbho@gmail.com>
2026-06-02 10:45:03 -0700

    Updated with a list of what predictors we will be looking at

--- 09c_sample-size.R
+++ 09c_sample-size.R
@@ -1,7 +1,7 @@
 ## --------------------------------------------------------------- ##
-# CAGED Raw data figures
+# CAGED Sample size by aquatic vs terrestrial data figures
 ## --------------------------------------------------------------- ##
-# Written by: 
+# Written by: Nico Matallana, Raine Detmer, Hillary Krumbholz
 
 ## ------------------------------------------- ##
 # Housekeeping ----
@@ -28,6 +28,24 @@ caged_beta <- read.csv(file.path("data", "08_caged_prepped-beta-dispersion.csv")
 dim(caged_beta)# 12905  rows
 
 ## ------------------------------------------- ##
-# Plots ---- 
+# Check  ---- 
 ## ------------------------------------------- ##
 colnames(caged_beta)
+
+# List the predictors
+caged_beta %>% select(starts_with("var")) %>% names()
+
+# [1] "var_upper.source"                       "var_climate.zone"                      
+# [3] "var_aq.or.terr"                         "var_ecotype1"                          
+# [5] "var_consumer.taxonomy"                  "var_consumer.metabolism"               
+# [7] "var_resource.type.category"             "var_consumer.richness.number"          
+# [9] "var_consumer.richness.category"         "var_max.consumer.size.category"        
+#[11] "var_dominant.consumer.species.category" "var_consumer.native.domestic"          
+#[13] "var_succ.vs.late"                       "var_exclusion.duration.continuousyears"
+#[15] "var_taxonomic.level"                    "var_exclosure.area.m2"                 
+#[17] "var_whereisthecage"
+
+# Check which columns are categorical and numerical 
+caged_beta %>% 
+  select(starts_with("var")) %>% 
+  sapply(class)
