855d8438a2fa71cc5ea9f2e8839469041f029a6d
njlyon0 <nicholasjlyon@gmail.com>
2026-06-02 14:43:23 -0400

    dev (join meta): save partial effort at integrate new LRRs

--- 07_attach-metadata.R
+++ 07_attach-metadata.R
@@ -1,205 +1,83 @@
-## --------------------------------------------------------------- ##
-# CAGED Attach Metadata
-## --------------------------------------------------------------- ##
-# Purpose:
-## Group members collectively filled out a metadata GoogleSheet manually
-## We want that attached to the data for use in visualization / analysis
-## This script accomplishes both that joining operation and some minor QC
-
-# Note: "source" and "exp.name" columns in metadata are created by "_boatyard_scripts/expand_metadata.R"
-# Note: Make sure to redownload site level metadata if it has changed in script 000
 
 ## ------------------------------------------- ##
-# Housekeeping ----
+# Load Gamma/Alpha/Dominance LRRs  ----
 ## ------------------------------------------- ##
+# Note we can skip (for now) 05 B-D outputs...
+## ...because the 06 variants have all relevant info plus LRRs
 
-# Load libraries
-librarian::shelf(tidyverse, supportR)
-
-# Create needed folders
-source(file = file.path("00_setup.R"))
-
-# Clear environment + collect garbage
-rm(list = ls()); gc()
-
-# Identify data files we want to add stuff to
-(w.meta_outs <- dir(path = file.path("data"), pattern = "05-A_caged_beta-disp_"))
-w.meta_in_list <- purrr::map(.x = w.meta_outs,
-                             .f = ~ read.csv(file = file.path("data", .x)))
-names(w.meta_in_list) <- w.meta_outs
-
-# Check structure of one
-dplyr::glimpse(w.meta_in_list[[1]])
-
-## ------------------------------------------- ##
-# Load Metadata ----
-## ------------------------------------------- ##
-
-# Read in the metadata
-meta_v1 <- read.csv(file = file.path("data", "sitelevel-metadata.csv"))
+# Read in gamma richness diffs
+gam.diff_v1 <- read.csv(file = file.path("data", "06-B_caged_gamma-diff.csv"))
 
 # Check structure
-dplyr::glimpse(meta_v1)
+dplyr::glimpse(gam.diff_v1)
 
-## ------------------------------------------- ##
-# Standardize Lat/Long Format ----
-## ------------------------------------------- ##
-
-# Look for non-numbers in lat/long columns
-supportR::num_check(data = meta_v1, col = c("var_lat", "var_long"))
-
-# Do needed repairs
-meta_v2 <- meta_v1 %>% 
-  # Rename relevant lat/long columns
-  dplyr::rename(lat = var_lat, long = var_long) %>%
-  # Replace M-dashes with hyphens
-  dplyr::mutate(dplyr::across(.cols = lat:long,
-                              .fns = ~ gsub(pattern = "−", replacement = "-", x = .)))
-
-# Re-check for non-numbers
-supportR::num_check(data = meta_v2, col = c("lat", "long"))
-
-# Check structure more generally
-dplyr::glimpse(meta_v2)
-
-## ------------------------------------------- ##
-# Standardize Free Text Columns ----
-## ------------------------------------------- ##
-
-# This is harder to do extensively but some coarse stuff makes sense
-meta_v3 <- meta_v2 %>% 
-  # Make some columns lowercase
-  dplyr::mutate(dplyr::across(.cols = c(ecotype2, target.consumer, nontarget.consumer,
-                                        resource.type.notes, consumer.trophic.level),
-                              .fns = ~ tolower(x = .)))
-
-# Re-check structure
-dplyr::glimpse(meta_v3)
-
-## ------------------------------------------- ##
-# Remove Unwanted Metadata Info ----
-## ------------------------------------------- ##
-
-# Needed processing
-meta_v4 <- meta_v3 %>% 
-  # Make all empty cells true NAs
-  dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
-                              .fns = ~ ifelse(nchar(.) == 0,
-                                              yes = NA, no = .))) %>% 
-  # Drop rows without either a source name or an experiment names
-  dplyr::filter(!is.na(source) | !is.na(exp.name)) %>% 
-  # Remove unwanted columns
-  dplyr::select(source, exp.name, lat, long, dplyr::starts_with("var")) %>% 
-  dplyr::select(-dplyr::contains("note")) %>% 
-  # Make lat/long data numeric
-  dplyr::mutate(lat = as.numeric(lat),
-    long = as.numeric(long)) %>% 
-  # Drop any columns that are entirely empty
-  dplyr::select(-dplyr::where(fn = ~ all(is.na(.))))
-
-# Check that only drops desired columns
-supportR::diff_check(old = names(meta_v3), new = names(meta_v4))
+# Load alpha diversity diffs
+alp.diff_v1 <- read.csv(file = file.path("data", "06-C_caged_alpha-div-diff_all-scales.csv"))
 
 # Check structure
-dplyr::glimpse(meta_v4)
+dplyr::glimpse(alp.diff_v1)
 
-## ------------------------------------------- ##
-# Check Metadata for Duplicate Rows ----
-## ------------------------------------------- ##
-
-# The join approach _requires_ that every source-exp.name combination have _ONLY one row!_
-# Count rows per source/exp.name
-meta_v4 %>% 
-  dplyr::group_by(source, exp.name) %>% 
-  dplyr::summarize(ct = dplyr::n(),
-    .groups = "drop") %>% 
-  dplyr::filter(ct > 1)
-
-# If this returns anything other than the following:
-### A tibble: 0 × 3
-### ℹ 3 variables: source <chr>, exp.name <chr>, ct <int>
-
-# THEN REVISIT THE METADATA
-## And delete/synonymize one of the two rows
-
-## ------------------------------------------- ##
-# Check Join Keys for Mismatches ----
-## ------------------------------------------- ##
-
-# Check for mismatches in which datasets are in the data but not metadata (or vice versa)
-supportR::diff_check(old = unique(c(w.meta_in_list[[1]]$source,
-                                    w.meta_in_list[[2]]$source)), 
-                     new = unique(meta_v3$source))
-
-# old = data, new = metadata
-# there will be some that are not in the metadata if we decided to exclude them 
-## If any are in data but not *metadata*:
-### Run "_boatyard_scripts/expand_metadata.R" and follow instructions at end of script
-
-## If any are in metadata but not *data*:
-### For some reason no beta dispersion was calculated for any spatial level
-### (likely lack of "exp.design" columns in original dataset)
-#### Check data key to confirm
-### (or potentially removed due to confounding treatments)
-
-# Remove any files not found in the data from the metadata
-meta_v5 <- dplyr::filter(.data = meta_v4, source %in% unique(c(w.meta_in_list[[1]]$source,
-                                                               w.meta_in_list[[2]]$source)))
-
-# Now check for mismatches in "exp.name" column
-## This is why this metadata is "site level"
-supportR::diff_check(old = unique(c(w.meta_in_list[[1]]$exp.name,
-                                    w.meta_in_list[[2]]$exp.name)),
-                     new = unique(meta_v5$exp.name))
-# old = data, new = metadata
-## If any are in data but not *metadata*:
-### The metadata had this info entered incorrectly
-### Open the GoogleSheet and edit the "exp.name" column as needed
-### Once done, start running this script again from the top to re-download the fixed version
-
-## If any are in metadata but not *data*:
-### Again, for some reason, no beta dispersion was calculated
-### Check original data and beta dispersion calculation script to debug
-### _OR_ could be caused by new "exp.name" in data and an outdated entry in the metaadata
-
-# Remove any experiment names not found in data
-meta_v6 <- dplyr::filter(.data = meta_v5, exp.name %in% unique(c(w.meta_in_list[[1]]$exp.name,
-                                                                 w.meta_in_list[[2]]$exp.name)))
-
-# Re-check that there are no mismatches
-supportR::diff_check(old = unique(c(w.meta_in_list[[1]]$source,
-                                    w.meta_in_list[[2]]$source)),
-                     new = unique(meta_v6$source))
-# old = data, new = metadata
-
-supportR::diff_check(old = unique(c(w.meta_in_list[[1]]$exp.name,
-                                    w.meta_in_list[[2]]$exp.name)),
-                                  new = unique(meta_v6$exp.name))
-# old = data, new = metadata
-
-## ------------------------------------------- ##
-# Load Gamma Richness Data ----
-## ------------------------------------------- ##
-
-# Read in gamma richness
-gamma_v1 <- read.csv(file = file.path("data", "05-B_caged_gamma-rich.csv"))
+# Load alpha diversity diffs
+dom.diff_v1 <- read.csv(file = file.path("data", "06-D_caged_dominance-diff_all-scales.csv"))
 
 # Check structure
-dplyr::glimpse(gamma_v1)
+dplyr::glimpse(dom.diff_v1)
 
 ## ------------------------------------------- ##
-# Load Mean Difference Data ----
+# Combine Gamma/Alpha/Dominance LRRs ----
+## ------------------------------------------- ##
+
+# Join these three together (to make later joins easier)
+diff_v1 <- gam.diff_v1 %>% 
+  dplyr::left_join(x = ., y = alp.diff_v1,
+    by = c("source", "organization", "site", "project.name", "sampling.years", 
+      "excluded.group", "measured.group", "exp.name")) %>% 
+  dplyr::left_join(x = ., y = dom.diff_v1,
+    by = c("source", "organization", "site", "project.name", "sampling.years", 
+    "excluded.group", "measured.group", "exp.name", "exp.design.4",
+    "exp.design.3", "exp.design.2", "exp.design.1",
+    "alpha.diversity_design.level" = "dominance_design.level")) %>% 
+  dplyr::relocate(dplyr::all_of(c(paste0("exp.design.", 1:4))), 
+    .after = exp.name) %>% 
+  dplyr::rename(betadisp.design.level = alpha.diversity_design.level)
+
+# Check structure
+dplyr::glimpse(diff_v1)
+
+## ------------------------------------------- ##
+# Beta - Load Mean Differences ----
 ## ------------------------------------------- ##
 
 # Read in the mean difference files too
-diff_v1 <- read.csv(file = file.path("data", "06_caged_mean-beta-diff_all-scales.csv"))
+beta.diff_v1 <- read.csv(file = file.path("data", "06-A_caged_mean-beta-diff_all-scales.csv"))
 
 # Check structure of one
-dplyr::glimpse(diff_v1)
+dplyr::glimpse(beta.diff_v1)
 
-# Read in log response ratio
-lrr_v1 <- read.csv(file = file.path("data", "06_caged_mean-lrr_all-scales.csv"))
+## ------------------------------------------- ##
+# Beta - Reformat Data ----
+## ------------------------------------------- ##
 
+# Get this data into a comparable format to the gamma/alpha/dominance data
+beta.diff_v2 <- beta.diff_v1 %>% 
+  tidyr::pivot_longer(cols = dplyr::starts_with("within.cage.treat")) %>% 
+  dplyr::mutate(name = gsub("within.cage.treat_", "", name)) %>% 
+  dplyr::mutate(new.name = paste0(name, "_", cage.treatment_std)) %>% 
+  dplyr::select(-name, -cage.treatment_std) %>% 
+  tidyr::pivot_wider(names_from = new.name, values_from = value) %>% 
+  dplyr::relocate(dplyr::contains(".mean"), dplyr::contains(".n"), 
+    dplyr::contains(".sd"), dplyr::contains(".se"), 
+    .after = dplyr::everything()) %>% 
+  dplyr::relocate(dplyr::contains("mean_"), dplyr::contains("mean.diff"), 
+    dplyr::contains("mean.lrr"),
+    .after = year) %>% 
+  dplyr::relocate(exp.name, betadisp.design.level, .after = year) %>% 
+  dplyr::select(-betadisp.mean.diff_uncaged, -betadisp.mean.lrr_uncaged) %>% 
+  dplyr::rename(betadisp.mean.treat.diff = betadisp.mean.diff_caged,
+    betadisp.mean.lrr = betadisp.mean.lrr_caged)
+
+# Check structure
+dplyr::glimpse(beta.diff_v2)
 
 ## ------------------------------------------- ##
 # Attach *EVERYTHING* to Data ----
@@ -227,10 +105,10 @@ for(focal_w.meta in w.meta_outs){
   
   # Now attach gamma richness & reorder columns
   w.meta_v3 <- w.meta_v2 %>% 
-    dplyr::left_join(y = gamma_v1, by = c("source", "organization", "site", 
-                                          "project.name", "sampling.years",
-                                          "excluded.group", "measured.group", 
-                                          "exp.name")) %>% 
+    dplyr::left_join(x = ., y = diff_v1, 
+      by = c("source", "organization", "site", "project.name", "sampling.years",
+      "excluded.group", "measured.group", "exp.name", "betadisp.design.level",
+      "exp.design.4", "exp.design.3", "exp.design.2", "exp.design.1")) 
     dplyr::relocate(gamma.richness, .before = exp.name)
   
   # Now attach summarized beta disp and mean difference
