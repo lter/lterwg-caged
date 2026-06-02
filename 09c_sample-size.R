c8da312ffb93bee752f5d2ef25cde718337f9d10
nicomatamej <nicomatamej@gmail.com>
2026-06-02 12:43:35 -0600

    Draft for loop for predictors sample size figures added

--- 09c_sample-size.R
+++ 09c_sample-size.R
@@ -49,3 +49,105 @@ caged_beta %>% select(starts_with("var")) %>% names()
 caged_beta %>% 
   select(starts_with("var")) %>% 
   sapply(class)
+
+
+
+
+# draft for loop for predictor sample sizes ----
+
+
+bd.prep = caged_beta[,c(2, 5, 20, 4, 6:19)] #simplify and reorder dataframe for plotting
+
+#colnames(beta.disper)
+
+colnames(bd.prep)
+
+fig.list = list()
+
+#hist(bd.temp$var_exclosure.area.m2)
+
+for(i in colnames(bd.prep)[4:length(colnames(bd.prep))]){
+  
+  #diagnostics, hash out before running
+  #i = colnames(bd.prep)[17]
+  
+  if(i == "var_exclosure.area.m2"){
+    bd.temp = bd.prep %>%
+      dplyr::select(exp.name, var_aq.or.terr, cage.treatment_std, all_of(i)) %>% # filter to relevant variables
+      drop_na() %>% # drop NAs
+      filter(.[[i]] != "") %>%
+      filter(.[[i]] != "unknown") %>%
+      mutate(var_exclosure.area.m2 = as.numeric(var_exclosure.area.m2))
+    
+  } else {
+    bd.temp = bd.prep %>%
+      dplyr::select(exp.name, var_aq.or.terr, cage.treatment_std, all_of(i)) %>% # filter to relevant variables
+      drop_na() %>% # drop NAs
+      filter(i != "") %>%
+      filter(i != "unknown") %>%
+      mutate(across(where(is.character), as.factor)) %>%
+      mutate(across(where(is.integer), as.numeric))
+  }
+  
+  
+  class(bd.temp[[i]])
+  
+  if(is.factor(bd.temp[[i]])){
+    
+    bd.factor.df = bd.temp %>%
+      group_by(var_aq.or.terr, cage.treatment_std, .[i]) %>%
+      summarise(n = n()) %>%
+      group_by(var_aq.or.terr, .[i]) %>%
+      summarise(avg.n = mean(n))
+    
+    
+    fig.list[[i]] = ggplot(bd.factor.df) +
+      geom_bar(aes(y = avg.n, x = var_aq.or.terr, fill = .data[[i]]), stat = "identity", position = position_dodge()) +
+      #labs(y = "Number of studies", fill = "Ecotype") +
+      theme(panel.background = element_blank(),
+            panel.border = element_rect(fill = NA, colour = "grey30"),
+            axis.title.x = element_blank() 
+      )
+    fig.list[[i]]
+    
+  } else {
+    
+    #i = colnames(bd.prep)[8]
+    
+    bd.num.df = bd.temp %>%
+      group_by(var_aq.or.terr, cage.treatment_std) %>%
+      mutate(sample_size = n()) %>%
+      ungroup() %>%
+      group_by(var_aq.or.terr) %>%
+      mutate(sample_size_mean = mean(sample_size)) %>%
+      ungroup()
+    
+    bd.num.sum = bd.num.df %>%
+      dplyr::select(var_aq.or.terr, sample_size_mean) %>%
+      distinct()
+    
+    fig.list[[i]] = ggplot(bd.num.df) +
+      geom_boxplot(aes(x = var_aq.or.terr, y = .data[[i]], fill = var_aq.or.terr), alpha = .5) +
+      #geom_point(aes(x = var_aq.or.terr, y = .data[[i]], color = var_aq.or.terr),
+      #position = position_jitter(), alpha = .05) +
+      scale_fill_manual(values = c("turquoise",
+                                   "darkgreen")) +
+      scale_color_manual(values = c("turquoise",
+                                    "darkgreen")) +
+      geom_text(data = bd.num.sum, aes(var_aq.or.terr, Inf, label = round(sample_size_mean)), vjust = 1) +
+      labs(x = "var_aq.or.terr", fill = "var_aq.or.terr")+
+      theme(panel.background = element_blank(),
+            panel.border = element_rect(fill = NA, colour = "grey30"),
+            axis.title.x = element_blank())
+    
+    fig.list[[i]]
+    
+  }
+  
+}
+
+length(fig.list)
+
+plot_grid(plotlist = fig.list, ncol = 3, nrow = 5)
+
+#
