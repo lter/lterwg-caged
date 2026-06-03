f2b361b92eb6e579058b660ce348446bd86be542
njlyon0 <nicholasjlyon@gmail.com>
2026-06-03 11:45:38 -0400

    fix (calc dom): make dominance aggregation sequentially sum from fine to coarse reps

--- 05-D_calc-dominance.r
+++ 05-D_calc-dominance.r
@@ -26,7 +26,7 @@ dplyr::glimpse(dom_v01)
 # Sum across years
 dom_v02 <- dom_v01 %>% 
   dplyr::group_by(dplyr::across(
-    dplyr::all_of(setdiff(x = names(.), y = c("year", "cage.treatment_orig", "abundance"))))) %>% 
+    dplyr::all_of(setdiff(x = names(.), y = c("year", "abundance"))))) %>% 
   dplyr::summarize(abundance = sum(abundance, na.rm = TRUE),
     .groups = "drop")
 
@@ -40,7 +40,7 @@ dplyr::glimpse(dom_v02)
 # Calculate dominance for "exp.design.1"
 dom_des1 <- dom_v02 %>% 
   dplyr::group_by(dplyr::across(
-    dplyr::all_of(setdiff(x = names(.), y = c("taxa", "abundance"))))) %>% 
+    dplyr::all_of(setdiff(x = names(.), y = c("cage.treatment_orig", "taxa", "abundance"))))) %>% 
   dplyr::summarize(total.abundance = sum(abundance, na.rm = TRUE),
     max.abundance = max(abundance, na.rm = TRUE),
     .groups = "drop") %>% 
@@ -60,10 +60,13 @@ dplyr::glimpse(dom_des1)
 # Calculate dominance for "exp.design.2"
 dom_des2 <- dom_v02 %>% 
   dplyr::group_by(dplyr::across(
-  dplyr::all_of(setdiff(x = names(.), y = c(paste0("exp.design.", 1), 
-    "taxa", "abundance"))))) %>% 
-  dplyr::summarize(total.abundance = sum(abundance, na.rm = TRUE),
-    max.abundance = max(abundance, na.rm = TRUE),
+    dplyr::all_of(setdiff(x = names(.), y = c("cage.treatment_orig", "abundance"))))) %>% 
+  dplyr::summarize(tax.abun1 = sum(abundance, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+  dplyr::all_of(setdiff(x = names(.), y = c("exp.design.1", "taxa", "tax.abun1"))))) %>% 
+  dplyr::summarize(total.abundance = sum(tax.abun1, na.rm = TRUE),
+    max.abundance = max(tax.abun1, na.rm = TRUE),
     .groups = "drop") %>% 
   dplyr::ungroup() %>% 
   dplyr::mutate(dominance = max.abundance / total.abundance,
@@ -81,10 +84,17 @@ dplyr::glimpse(dom_des2)
 # Calculate dominance for "exp.design.3"
 dom_des3 <- dom_v02 %>% 
   dplyr::group_by(dplyr::across(
-  dplyr::all_of(setdiff(x = names(.), y = c(paste0("exp.design.", 1:2), 
-    "taxa", "abundance"))))) %>% 
-  dplyr::summarize(total.abundance = sum(abundance, na.rm = TRUE),
-    max.abundance = max(abundance, na.rm = TRUE),
+    dplyr::all_of(setdiff(x = names(.), y = c("cage.treatment_orig", "abundance"))))) %>% 
+  dplyr::summarize(tax.abun1 = sum(abundance, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+  dplyr::all_of(setdiff(x = names(.), y = c("exp.design.1", "tax.abun1"))))) %>% 
+  dplyr::summarize(tax.abun2 = sum(tax.abun1, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+  dplyr::all_of(setdiff(x = names(.), y = c("exp.design.2",  "taxa", "tax.abun2"))))) %>% 
+  dplyr::summarize(total.abundance = sum(tax.abun2, na.rm = TRUE),
+    max.abundance = max(tax.abun2, na.rm = TRUE),
     .groups = "drop") %>% 
   dplyr::ungroup() %>% 
   dplyr::mutate(dominance = max.abundance / total.abundance,
@@ -102,10 +112,21 @@ dplyr::glimpse(dom_des3)
 # Calculate dominance for "exp.design.4"
 dom_des4 <- dom_v02 %>% 
   dplyr::group_by(dplyr::across(
-  dplyr::all_of(setdiff(x = names(.), y = c(paste0("exp.design.", 1:3), 
-    "taxa", "abundance"))))) %>% 
-  dplyr::summarize(total.abundance = sum(abundance, na.rm = TRUE),
-    max.abundance = max(abundance, na.rm = TRUE),
+    dplyr::all_of(setdiff(x = names(.), y = c("cage.treatment_orig", "abundance"))))) %>% 
+  dplyr::summarize(tax.abun1 = sum(abundance, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+  dplyr::all_of(setdiff(x = names(.), y = c("exp.design.1", "tax.abun1"))))) %>% 
+  dplyr::summarize(tax.abun2 = sum(tax.abun1, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+  dplyr::all_of(setdiff(x = names(.), y = c("exp.design.2", "tax.abun2"))))) %>% 
+  dplyr::summarize(tax.abun3 = sum(tax.abun2, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+  dplyr::all_of(setdiff(x = names(.), y = c("exp.design.3", "taxa", "tax.abun3"))))) %>% 
+  dplyr::summarize(total.abundance = sum(tax.abun3, na.rm = TRUE),
+    max.abundance = max(tax.abun3, na.rm = TRUE),
     .groups = "drop") %>% 
   dplyr::ungroup() %>% 
   dplyr::mutate(dominance = max.abundance / total.abundance,
@@ -123,10 +144,25 @@ dplyr::glimpse(dom_des4)
 # Calculate dominance for "exp.design.4"
 dom_name <- dom_v02 %>% 
   dplyr::group_by(dplyr::across(
-  dplyr::all_of(setdiff(x = names(.), y = c(paste0("exp.design.", 1:4), 
-    "taxa", "abundance"))))) %>% 
-  dplyr::summarize(total.abundance = sum(abundance, na.rm = TRUE),
-    max.abundance = max(abundance, na.rm = TRUE),
+    dplyr::all_of(setdiff(x = names(.), y = c("cage.treatment_orig", "abundance"))))) %>% 
+  dplyr::summarize(tax.abun1 = sum(abundance, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+  dplyr::all_of(setdiff(x = names(.), y = c("exp.design.1", "tax.abun1"))))) %>% 
+  dplyr::summarize(tax.abun2 = sum(tax.abun1, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+  dplyr::all_of(setdiff(x = names(.), y = c("exp.design.2", "tax.abun2"))))) %>% 
+  dplyr::summarize(tax.abun3 = sum(tax.abun2, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+  dplyr::all_of(setdiff(x = names(.), y = c("exp.design.3", "tax.abun3"))))) %>% 
+  dplyr::summarize(tax.abun4 = sum(tax.abun3, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+  dplyr::all_of(setdiff(x = names(.), y = c("exp.design.4", "taxa", "tax.abun4"))))) %>% 
+  dplyr::summarize(total.abundance = sum(tax.abun4, na.rm = TRUE),
+    max.abundance = max(tax.abun4, na.rm = TRUE),
     .groups = "drop") %>% 
   dplyr::ungroup() %>% 
   dplyr::mutate(dominance = max.abundance / total.abundance,
