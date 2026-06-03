b40e73342755ef8bdd355cfab3d641cdbb44efcf
Jamie McDevitt-Irwin <jamie.mcirwin@gmail.com>
2026-06-03 15:26:55 -0700

    new data files

--- 09_prepare-for-stats.R
+++ 09_prepare-for-stats.R
@@ -24,7 +24,7 @@ rm(list = ls()); gc()
 ## ------------------------------------------- ##
 
 # Read in relevant data file
-betadisp_v1 <- read.csv(file.path("data", "08_caged_w.meta-beta-disp_finest-scales.csv"))
+betadisp_v1 <- read.csv(file.path("data", "08-A_caged_w.meta-beta-disp_all-scales.csv"))
 effectsizes_v1 <- read.csv(file.path("data", "08_caged_experiment-level-everything_fine-scales.csv"))
 
 
@@ -34,21 +34,26 @@ dplyr::glimpse(effectsizes_v1)
 
 
 # Check number of sources
-unique(effectsizes_v1$source) # 117
-unique(effectsizes_v1$exp.name) # 347
+unique(effectsizes_v1$source) # 118
+unique(effectsizes_v1$exp.name) # 356
 
 unique(betadisp_v1$source) # 117
 unique(betadisp_v1$exp.name) # 347
 
 # Check number of rows and dataframe
-dim(effectsizes_v1) # 1286
-dim(effectsizes_v1) # 1286 
+dim(effectsizes_v1) # 1556   43
+dim(effectsizes_v1) # 1556   43
 # so the effect size needs to be set to only unique 
+
+dim(betadisp_v1) # 13964    42
+dim(betadisp_v1) # 13964    42
 
 colnames(effectsizes_v1)
 
 effectsizes_v2 <- effectsizes_v1 %>%
   select(source, exp.name, lat,var_succ.vs.late,
+         var_upper.source, gamma.richness_exp.name,
+         var_aq.or.terr,var_exclusion.duration.continuousyears,
          contains("lrr")) %>%
 distinct()
 
