f12ef6c8bcc462514ba872e314f4f5dd4d4224cf
njlyon0 <nicholasjlyon@gmail.com>
2026-06-03 11:20:48 -0400

    feat (alpha diff): change to experiment name averages #43

--- 06-C_compare-treat-alpha.R
+++ 06-C_compare-treat-alpha.R
@@ -27,64 +27,56 @@ dplyr::glimpse(alp.diff_v01)
 # Streamline / Prepare Data ----
 ## ------------------------------------------- ##
 
-# Filter to only desired treatments and get data in right shape
+# Filter to only desired treatments and average across replicates
 alp.diff_v02 <- alp.diff_v01 %>% 
+  dplyr::filter(alpha.diversity_design.level == "exp.name") %>% 
   dplyr::filter(cage.treatment_std %in% c("caged", "uncaged")) %>% 
-  dplyr::mutate(cage.treatment_std = paste0("alpha.diversity_", cage.treatment_std)) %>% 
-  tidyr::pivot_wider(names_from = cage.treatment_std,
-    values_from = alpha.diversity_richness)
+  dplyr::group_by(dplyr::across(
+    dplyr::all_of(setdiff(x = names(.), y = c("exp.design.1", "alpha.diversity_richness"))))) %>% 
+  dplyr::summarize(alpha.div = mean(alpha.diversity_richness, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::mutate(cage.treatment_std = paste0("alpha.diversity_", cage.treatment_std))
 
 # Check structure
 dplyr::glimpse(alp.diff_v02)
 
-# Average across caged and uncaged 
+## ------------------------------------------- ##
+# Summarize to Experiment Name ----
+## ------------------------------------------- ##
 
-# Identify the grouping columns 
-diff_groupcols <- c("source", "organization", "site", 
-                    "excluded.group", "measured.group", 
-                    "exp.name", "alpha.diversity_design.level", 
-                    "alpha.diversity_caged", "alpha.diversity_uncaged")
-
-# Do some needed preparatory calculatation
+# Average (in order) across design replicates up to experiment name
 alp.diff_v03 <- alp.diff_v02 %>% 
-  # Summarize within treatments/etc.
   dplyr::group_by(dplyr::across(
-    dplyr::all_of(c(diff_groupcols)))) %>%  
-  dplyr::summarize(alpha.diversity_caged.mean = mean(alpha.diversity_caged, na.rm = TRUE),
-                   alpha.diversity_uncaged.mean = mean(alpha.diversity_uncaged, na.rm = TRUE),
-                   .groups = "drop") %>%
-  dplyr::select(-alpha.diversity_caged, -alpha.diversity_uncaged)
+    dplyr::all_of(setdiff(x = names(.), y = c("exp.design.2", "alpha.div"))))) %>% 
+  dplyr::summarize(alpha.div2 = mean(alpha.div, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+    dplyr::all_of(setdiff(x = names(.), y = c("exp.design.3", "alpha.div2"))))) %>% 
+  dplyr::summarize(alpha.div3 = mean(alpha.div2, na.rm = TRUE),
+    .groups = "drop") %>% 
+  dplyr::group_by(dplyr::across(
+    dplyr::all_of(setdiff(x = names(.), y = c("exp.design.4", "alpha.div3"))))) %>% 
+  dplyr::summarize(alpha.diversity_richness = mean(alpha.div3, na.rm = TRUE),
+    .groups = "drop")
 
-dim(alp.diff_v03)
-  
-
-# minimum value of alpha diversity
-range(alp.diff_v03$alpha.diversity_uncaged.mean) # has NA's because some scales you can't calculate? 
-
-alp.diff_v03 %>% 
-  summarise(min_val = min(alpha.diversity_uncaged.mean, na.rm = TRUE)) # 1
-
-alp.diff_v03 %>% 
-  summarise(min_val = min(alpha.diversity_caged.mean, na.rm = TRUE)) # 1
+# Check structure
+dplyr::glimpse(alp.diff_v03)
 
 ## ------------------------------------------- ##
-# Calculate LRR ----
+# Reshape & Calculate LRR ----
 ## ------------------------------------------- ##
 
-# Calculate LRR
+# Reshape the data and calculate log response ratio
 alp.diff_v04 <- alp.diff_v03 %>% 
-  # dplyr::mutate(dplyr::across(.cols = dplyr::ends_with("caged"),
-  #   .fns = ~ ifelse(is.na(.), yes = 0, no = .))) %>% 
-  # add minimum value of alpha diversity - 1
-  dplyr::mutate(caged = alpha.diversity_caged.mean + 1,
-    uncaged = alpha.diversity_uncaged.mean + 1) %>% 
-  # create log response ratio
-  dplyr::mutate(within.cage.treat_alpha.mean.lrr = log2(uncaged / caged)) %>% 
-  dplyr::select(-caged, -uncaged) %>%
-  # remove NA's
-  drop_na(within.cage.treat_alpha.mean.lrr)
-
-dim(alp.diff_v04)
+  tidyr::pivot_wider(names_from = cage.treatment_std,
+    values_from = alpha.diversity_richness) %>% 
+  dplyr::mutate(dplyr::across(.cols = dplyr::ends_with("caged"),
+    .fns = ~ ifelse(is.na(.), yes = 0, no = .))) %>% 
+  dplyr::mutate(caged = alpha.diversity_caged + 1,
+    uncaged = alpha.diversity_uncaged + 1) %>% 
+  dplyr::mutate(alpha.diversity_cage.treat.diff = uncaged - caged,
+    alpha.diversity_cage.treat.lrr = log2(uncaged / caged)) %>% 
+  dplyr::select(-caged, -uncaged)
 
 # Check structure
 dplyr::glimpse(alp.diff_v04)
