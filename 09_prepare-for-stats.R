ab76f2178e6e038b26f17d343871dfc5d64550f5
Jamie McDevitt-Irwin <jamie.mcirwin@gmail.com>
2026-06-03 16:21:26 -0700

    making sure the right files are going in

--- 09_prepare-for-stats.R
+++ 09_prepare-for-stats.R
@@ -24,7 +24,7 @@ rm(list = ls()); gc()
 ## ------------------------------------------- ##
 
 # Read in relevant data file
-betadisp_v1 <- read.csv(file.path("data", "08-A_caged_w.meta-beta-disp_all-scales.csv"))
+betadisp_v1 <- read.csv(file.path("data", "08-A_caged_w.meta-beta-disp_finest-scales.csv"))
 effectsizes_v1 <- read.csv(file.path("data", "08_caged_experiment-level-everything_fine-scales.csv"))
 
 
@@ -34,15 +34,16 @@ dplyr::glimpse(effectsizes_v1)
 
 
 # Check number of sources
-unique(effectsizes_v1$source) # 118
-unique(effectsizes_v1$exp.name) # 356
+unique(effectsizes_v1$source) # 117
+unique(effectsizes_v1$exp.name) # 347
+# now this is fixed? 
 
 unique(betadisp_v1$source) # 117
 unique(betadisp_v1$exp.name) # 347
 
 # Check number of rows and dataframe
-dim(effectsizes_v1) # 1556   43
-dim(effectsizes_v1) # 1556   43
+dim(effectsizes_v1) # 694  46
+dim(effectsizes_v1) # 694  46
 # so the effect size needs to be set to only unique 
 
 dim(betadisp_v1) # 13964    42
